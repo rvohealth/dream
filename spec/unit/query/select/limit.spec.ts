dfd4f0f6b9cdc4918a18c3347d6fac9f98c3d037
avocadojesus <jgarbey@gmail.com>
2023-04-03 17:43:01 -0700

    add gap specs for existing implementations

---
+++ spec/unit/query/select/limit.spec.ts
@@ -0,0 +1,12 @@
+import User from '../../../../src/test-app/app/models/user'
+
+describe('SelectQuery#limit', () => {
+  it('limits number of records returned', async () => {
+    const user1 = await User.create({ email: 'fred@frewd', password: 'howyadoin' })
+    const user2 = await User.create({ email: 'how@yadoin', password: 'howyadoin' })
+
+    const records = await User.order('id').limit(2).all()
+    expect(records[0].id).toEqual(user1.id)
+    expect(records[1].id).toEqual(user2.id)
+  })
+})
---
+++ spec/unit/query/select/order.spec.ts
@@ -0,0 +1,12 @@
+import User from '../../../../src/test-app/app/models/user'
+
+describe('SelectQuery#order', () => {
+  it('orders records by id', async () => {
+    const user1 = await User.create({ email: 'fred@frewd', password: 'howyadoin' })
+    const user2 = await User.create({ email: 'how@yadoin', password: 'howyadoin' })
+
+    const records = await User.limit(2).order('id', 'desc').all()
+    expect(records[0].id).toEqual(user2.id)
+    expect(records[1].id).toEqual(user1.id)
+  })
+})
