f4f03a6fd8c02d65b516592c72608601ce9a4521
avocadojesus <jgarbey@gmail.com>
2023-04-08 07:49:27 -0700

    add recursive flag

--- boot/copy-boilerplate.ts
+++ boot/copy-boilerplate.ts
@@ -2,7 +2,7 @@ import sspawn from '../src/helpers/sspawn'
 
 export default async function copyBoilerplate() {
   console.log('copying boilerplate to sync folder...')
-  await sspawn('cp ./boot/boilerplate/sync ./src/sync')
+  await sspawn('cp -R ./boot/boilerplate/sync ./src/sync')
   console.log('done!')
 }
 
