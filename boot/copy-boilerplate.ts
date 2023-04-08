9fad7ef2cce78f51c931b81a08f8f8e81fd04bd8
avocadojesus <jgarbey@gmail.com>
2023-04-08 08:29:35 -0700

    copying boilerplate only copies what it needs to

--- boot/copy-boilerplate.ts
+++ boot/copy-boilerplate.ts
@@ -1,9 +1,28 @@
+import * as fs from 'fs/promises'
 import sspawn from '../src/helpers/sspawn'
 
 export default async function copyBoilerplate() {
-  console.log('copying boilerplate to sync folder...')
-  await sspawn('cp -R ./boot/boilerplate/sync ./src/sync')
-  console.log('done!')
+  console.log('checking for sync folder compatibility...')
+  try {
+    await fs.statfs('./src/sync/schema.ts')
+  } catch (_) {
+    console.log('schema file missing, adding boilerplate schema file')
+    await sspawn('cp ./boot/boilerplate/sync/schema.ts ./src/sync/schema.ts')
+  }
+
+  try {
+    await fs.statfs('./src/sync/dream.ts')
+  } catch (_) {
+    console.log('dream file missing, adding boilerplate dream file')
+    await sspawn('cp ./boot/boilerplate/sync/dream.ts ./src/sync/dream.ts')
+  }
+
+  try {
+    await fs.statfs('./src/sync/models.ts')
+  } catch (_) {
+    console.log('models file missing, adding boilerplate models file')
+    await sspawn('cp ./boot/boilerplate/sync/models.ts ./src/sync/models.ts')
+  }
 }
 
 copyBoilerplate()
