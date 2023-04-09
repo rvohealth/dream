9fc3c2767b5879a3aa286e03cc02e86830696baa
avocadojesus <jgarbey@gmail.com>
2023-04-08 20:45:45 -0700

    add debug

--- src/bin/migrate.ts
+++ src/bin/migrate.ts
@@ -6,22 +6,18 @@ import { loadDreamYamlFile } from '../helpers/path'
 
 async function migrateToLatest() {
   const yamlConf = await loadDreamYamlFile()
-  console.log(
-    'FROM MIGRATION',
-    process.env.CORE_DEVELOPMENT,
+  const migrationFolder =
     process.env.CORE_DEVELOPMENT === '1'
       ? path.join(__dirname, '..', 'test-app', 'db', 'migrations')
       : path.join(__dirname, '..', '..', '..', '..', yamlConf.migrations_path)
-  )
+  console.log('MIGRATION STUFF', migrationFolder)
+
   const migrator = new Migrator({
     db,
     provider: new FileMigrationProvider({
       fs,
       path,
-      migrationFolder:
-        process.env.CORE_DEVELOPMENT === '1'
-          ? path.join(__dirname, '..', 'test-app', 'db', 'migrations')
-          : path.join(__dirname, '..', '..', '..', '..', yamlConf.migrations_path),
+      migrationFolder,
     }),
   })
 
