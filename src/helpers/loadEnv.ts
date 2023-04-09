bc6ccffd4a4998a7cb2b3568f7d364dcd81570a4
avocadojesus <jgarbey@gmail.com>
2023-04-08 19:47:56 -0700

    add different file path for consuming apps

--- src/helpers/loadEnv.ts
+++ src/helpers/loadEnv.ts
@@ -1,2 +1,7 @@
 import * as dotenv from 'dotenv'
-dotenv.config({ path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env' })
+
+if (process.env.CORE_DEVELOPMENT === '1') {
+  dotenv.config({ path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env' })
+} else {
+  dotenv.config({ path: process.env.NODE_ENV === 'test' ? '../../.env.test' : '../../.env' })
+}
