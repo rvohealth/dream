19934361bbdb9b91839f83402a89d52a1b813e66
avocadojesus <jgarbey@gmail.com>
2023-04-12 11:50:02 -0700

    don't rely on models for repl

--- test-app/conf/repl.ts
+++ test-app/conf/repl.ts
@@ -1,9 +1,24 @@
 import * as repl from 'node:repl'
+import * as fs from 'fs/promises'
+import * as path from 'path'
 
 const replServer = repl.start('> ')
 export default (async function () {
-  const models = (await import('../../src/sync/models')).default
-  Object.values(models).forEach(ModelClass => {
-    replServer.context[(ModelClass as any).name] = ModelClass
-  })
+  const dreamPaths = await getFiles('./test-app/app/models')
+  for (const dreamPath of dreamPaths) {
+    const importablePath = dreamPath.replace(/.*\/test-app/, '..')
+    const DreamClass = (await import(importablePath)).default
+    replServer.context[(DreamClass as any).name] = DreamClass
+  }
 })()
+
+async function getFiles(dir: string): Promise<string[]> {
+  const dirents = await fs.readdir(dir, { withFileTypes: true })
+  const files = await Promise.all(
+    dirents.map(dirent => {
+      const res = path.resolve(dir, dirent.name)
+      return dirent.isDirectory() ? getFiles(res) : res
+    })
+  )
+  return Array.prototype.concat(...files)
+}
