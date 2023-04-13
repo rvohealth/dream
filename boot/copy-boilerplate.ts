8028a82824a3f2f4c6c3a1199ea3820f451b8786
avocadojesus <jgarbey@gmail.com>
2023-04-13 12:30:17 -0700

    temporarily comment out implementation for debugging purposes

--- boot/copy-boilerplate.ts
+++ boot/copy-boilerplate.ts
@@ -3,34 +3,35 @@ import sspawn from '../src/helpers/sspawn'
 
 export default async function copyBoilerplate() {
   console.log('checking for sync folder compatibility...')
+  console.log('TESTING')
 
-  try {
-    await fs.statfs('./src/sync')
-  } catch (_) {
-    console.log('schema folder missing, adding now')
-    await sspawn('mkdir ./src/sync')
-  }
+  // try {
+  //   await fs.statfs('./src/sync')
+  // } catch (_) {
+  //   console.log('schema folder missing, adding now')
+  //   await sspawn('mkdir ./src/sync')
+  // }
 
-  try {
-    await fs.statfs('./src/sync/schema.ts')
-  } catch (_) {
-    console.log('schema file missing, adding boilerplate schema file')
-    await sspawn('cp ./boot/boilerplate/sync/schema.ts ./src/sync/schema.ts')
-  }
+  // try {
+  //   await fs.statfs('./src/sync/schema.ts')
+  // } catch (_) {
+  //   console.log('schema file missing, adding boilerplate schema file')
+  //   await sspawn('cp ./boot/boilerplate/sync/schema.ts ./src/sync/schema.ts')
+  // }
 
-  try {
-    await fs.statfs('./src/sync/dream.ts')
-  } catch (_) {
-    console.log('dream file missing, adding boilerplate dream file')
-    await sspawn('cp ./boot/boilerplate/sync/dream.ts ./src/sync/dream.ts')
-  }
+  // try {
+  //   await fs.statfs('./src/sync/dream.ts')
+  // } catch (_) {
+  //   console.log('dream file missing, adding boilerplate dream file')
+  //   await sspawn('cp ./boot/boilerplate/sync/dream.ts ./src/sync/dream.ts')
+  // }
 
-  try {
-    await fs.statfs('./src/sync/associations.ts')
-  } catch (_) {
-    console.log('associations file missing, adding boilerplate dream file')
-    await sspawn('cp ./boot/boilerplate/sync/associations.ts ./src/sync/associations.ts')
-  }
+  // try {
+  //   await fs.statfs('./src/sync/associations.ts')
+  // } catch (_) {
+  //   console.log('associations file missing, adding boilerplate dream file')
+  //   await sspawn('cp ./boot/boilerplate/sync/associations.ts ./src/sync/associations.ts')
+  // }
 }
 
 copyBoilerplate()
