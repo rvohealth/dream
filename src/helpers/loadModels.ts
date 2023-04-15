a6a0b6eee00b9cedfc435c4bdf053e58efb55e90
avocadojesus <jgarbey@gmail.com>
2023-04-14 21:25:55 -0700

    use relative path for loading models

--- src/helpers/loadModels.ts
+++ src/helpers/loadModels.ts
@@ -1,19 +1,25 @@
 import * as path from 'path'
 import { promises as fs } from 'fs'
 import Dream from '../dream'
-import { modelsPath } from './path'
+import { loadDreamYamlFile, modelsPath } from './path'
 
 export default async function loadModels() {
   const pathToModels = await modelsPath()
-  console.log('PATH TO MODELS', pathToModels, process.cwd())
+  const yamlConf = await loadDreamYamlFile()
   const modelPaths = await fs.readdir(pathToModels)
   const models: { [key: string]: typeof Dream } = {}
 
   for (const modelPath of modelPaths.filter(path => /\.ts$/.test(path))) {
     const fullPath = path.join(pathToModels, modelPath)
+    const relativePath =
+      `../../${yamlConf.models_path}/` +
+      fullPath
+        .replace(new RegExp(`^${process.cwd()}`, ''), '')
+        .replace(new RegExp(`^\/${yamlConf.models_path}\/`), '')
+
     let ModelClass: typeof Dream | null = null
     try {
-      ModelClass = (await import(fullPath)).default
+      ModelClass = (await import(relativePath)).default
     } catch (error) {
       console.log(`Failed to import the following file: ${fullPath}. Error: ${error}`)
     }
@@ -22,4 +28,15 @@ export default async function loadModels() {
   }
 
   return models
+}
+
+async function getFiles(dir: string): Promise<string[]> {
+  const dirents = await fs.readdir(dir, { withFileTypes: true })
+  const files = await Promise.all(
+    dirents.map(dirent => {
+      const res = path.resolve(dir, dirent.name)
+      return dirent.isDirectory() ? getFiles(res) : res
+    })
+  )
+  return Array.prototype.concat(...files)
 }
