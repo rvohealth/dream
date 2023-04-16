70f97bcc5fba936ea5df510b465983ff100cba2d
avocadojesus <jgarbey@gmail.com>
2023-04-16 16:18:42 -0700

    add debug

--- boot/cli.ts
+++ boot/cli.ts
@@ -117,6 +117,7 @@ program
   .option('--core', 'sets core to true')
   .action(async () => {
     setCoreDevelopmentFlag(program.args)
+    console.log('DEBUG!!!', program.args)
     await sspawn(`npx ts-node boot/sync-existing-or-create-boilerplate.ts`)
   })
 
