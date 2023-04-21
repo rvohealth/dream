09fb9433c32b66cd73f0f8510a28e8e5a0bd7165
Daniel Nelson <844258+daniel-nelson@users.noreply.github.com>
2023-04-21 17:10:53 -0500

    Clean up new test app models

--- test-app/app/models/rating.ts
+++ test-app/app/models/rating.ts
@@ -1,15 +1,7 @@
-import { HasOne } from '../../../src'
 import BelongsTo from '../../../src/decorators/associations/belongs-to'
-import HasMany from '../../../src/decorators/associations/has-many'
 import { Column } from '../../../src/decorators/column'
-import AfterCreate from '../../../src/decorators/hooks/after-create'
-import AfterSave from '../../../src/decorators/hooks/after-save'
-import AfterUpdate from '../../../src/decorators/hooks/after-update'
-import BeforeCreate from '../../../src/decorators/hooks/before-create'
 import dream from '../../../src/dream'
 import Composition from './composition'
-import CompositionAsset from './composition-asset'
-import CompositionAssetAudit from './composition-asset-audit'
 import Post from './post'
 import User from './user'
 
@@ -31,8 +23,6 @@ export default class Rating extends Dream {
   public user: User
 
   @BelongsTo(['compositions', 'posts'], () => [Composition, Post], {
-    // @BelongsTo(['compositions'], () => [Composition], {
-    // @BelongsTo(['posts'], () => [Composition], {
     foreignKey: 'rateable_id',
     polymorphic: true,
   })
