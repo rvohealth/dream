6e8b1c9727a2c89d9adb23f8c037602593935a62
Daniel Nelson <844258+daniel-nelson@users.noreply.github.com>
2023-04-25 08:51:19 -0500

    Need both custom matchers in the same declaration

--- spec/setup/hooks.ts
+++ spec/setup/hooks.ts
@@ -1,6 +1,5 @@
 import truncate from '../helpers/truncate'
 import './jestMatchers/dreamModel'
-import './jestMatchers/dreamModels'
 
 beforeEach(async () => {
   await truncate()
--- spec/setup/jestMatchers/dreamModel.ts
+++ spec/setup/jestMatchers/dreamModel.ts
@@ -19,6 +19,7 @@ declare global {
   namespace jest {
     interface Matchers<R> {
       toMatchDreamModel(expected: any): CustomMatcherResult
+      toMatchDreamModels(expected: any): CustomMatcherResult
     }
   }
 }
@@ -58,6 +59,47 @@ expect.extend({
     // NOT to match
     // ${JSON.stringify(attributes(expected), null, 2)}`,
     //     }
+  },
+
+  toMatchDreamModels(received: any, expected: any) {
+    if (received?.constructor !== Array) {
+      return {
+        pass: false,
+        message: () => `Expected received object to be an Array, but was ${received?.constructor?.name}`,
+      }
+    } else if (expected?.constructor !== Array) {
+      return {
+        pass: false,
+        message: () => `Expected expected object to be an Array, but was ${expected?.constructor?.name}`,
+      }
+    } else if (expected.length != received.length) {
+      return {
+        pass: false,
+        message: () =>
+          `Expected arrays of the same length, but expected has ${expected.length} elements and received has ${received.length}`,
+      }
+    } else if (expected.length === 0) {
+      return {
+        pass: true,
+        message: () => 'Expected arrays not to match, but both were empty',
+      }
+    }
+
+    let results: jest.CustomMatcherResult
+
+    received.forEach((receivedElement, i) => {
+      results = expectMatchingDreamModels(receivedElement, expected[i], 'toMatchDreamModels')
+      if (!results.pass) return
+    })
+
+    if (results!.pass) {
+      return {
+        pass: true,
+        message: () => 'Expected arrays of Dream objects not to match, but they do',
+      }
+    } else {
+      return results!
+    }
   },
 })
 
