bc8c872743cf555a677d26e39c83cac6dd75b408
avocadojesus <jgarbey@gmail.com>
2023-04-28 00:26:07 -0700

    update scope decorator to use new decorator pattern

--- src/decorators/scope.ts
+++ src/decorators/scope.ts
@@ -5,14 +5,17 @@ export default function Scope(
 ): any {
   return function (target: any, key: string, _: any) {
     const branch = opts.default ? 'default' : 'named'
-    Object.assign(target.scopes[branch], [
-      ...target.scopes[branch],
-      {
-        className: target.name,
-        method: key,
-        default: opts.default || false,
-      },
-    ])
+    if (!Object.getOwnPropertyDescriptor(target, 'scopes'))
+      target['scopes'] = {
+        default: [] as ScopeStatement[],
+        named: [] as ScopeStatement[],
+      }
+
+    target.scopes[branch].push({
+      className: target.name,
+      method: key,
+      default: opts.default || false,
+    })
   }
 }
 
