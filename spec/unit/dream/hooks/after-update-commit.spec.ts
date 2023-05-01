8df341468c6137f5bfaeedf5f15e5b4478f88900
avocadojesus <jgarbey@gmail.com>
2023-05-01 16:36:53 -0700

    remove comments

--- spec/unit/dream/hooks/after-update-commit.spec.ts
+++ spec/unit/dream/hooks/after-update-commit.spec.ts
@@ -6,8 +6,6 @@ describe('Dream AfterUpdateCommit decorator', () => {
   it('runs the query after the transactions have been commited', async () => {
     const user = await User.create({ email: 'fred@frewd', password: 'howyadoin' })
 
-    // setting the content field to "override on commit" will cause the composition model to set
-    // different content on this record on both AfterUpdate, and AfterUpdateCommit.
     const composition = await Composition.create({
       user_id: user.id,
     })
@@ -23,8 +21,6 @@ describe('Dream AfterUpdateCommit decorator', () => {
       await Dream.transaction(async txn => {
         const user = await User.create({ email: 'fred@frewd', password: 'howyadoin' }, txn)
 
-        // setting the content field to "override on commit" will cause the composition model to set
-        // different content on this record on both AfterUpdate, and AfterUpdateCommit.
         composition = await Composition.create({ user_id: user.id }, txn)
         await composition.update({
           content: 'change me after update commit',
