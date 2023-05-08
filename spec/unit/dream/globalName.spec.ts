a18733af490334b505d4881c47a4c68fd02f060f
avocadojesus <jgarbey@gmail.com>
2023-05-08 12:51:01 -0700

    add nested case and base case for api clarity

--- spec/unit/dream/globalName.spec.ts
+++ spec/unit/dream/globalName.spec.ts
@@ -1,7 +1,14 @@
 import Mylar from '../../../test-app/app/models/Balloon/Mylar'
+import User from '../../../test-app/app/models/User'
 
 describe('Dream.globalName', () => {
   it('returns the class name', async () => {
-    expect(await Mylar.globalName()).toEqual('BalloonMylar')
+    expect(await User.globalName()).toEqual('User')
+  })
+
+  context('with a nested class', () => {
+    it('combines the path parts together to make a global-safe version of the class', async () => {
+      expect(await Mylar.globalName()).toEqual('BalloonMylar')
+    })
   })
 })
