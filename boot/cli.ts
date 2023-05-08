372e26d7018b971e82eb6e10f4fbb53c8416beb7
avocadojesus <jgarbey@gmail.com>
2023-05-08 15:56:55 -0700

    temporarily restore old implementation for debugging

--- boot/cli.ts
+++ boot/cli.ts
@@ -108,8 +108,15 @@ program
     const coreDevFlag = setCoreDevelopmentFlag(program.args)
     const stepArg = program.args.find(arg => /step=\d+/.test(arg))
     const step = stepArg ? parseInt(stepArg!.replace('--step=', '')) : 1
-    await sspawn(`${coreDevFlag}npx ts-node src/bin/db-rollback.ts ${step}`)
-    await sspawn(yarncmdRunByAppConsumer('dream sync:types', program.args))
+    console.log('RUNNING: ', `${coreDevFlag}npx ts-node src/bin/db-rollback.ts ${step}`)
+    // await sspawn(`${coreDevFlag}npx ts-node src/bin/db-rollback.ts ${step}`)
+    // await sspawn(yarncmdRunByAppConsumer('dream sync:types', program.args))
+
+    await sspawn(
+      `${coreDevFlag}npx ts-node src/bin/db-rollback.ts ${step} && ${
+        process.env.DREAM_CORE_DEVELOPMENT === '1' ? 'yarn dream sync:types --core' : 'yarn dream sync:types'
+      }`
+    )
   })
 
 program
