ee13c5eca977077d2c78e13820ee8698a27ac2d5
avocadojesus <jgarbey@gmail.com>
2023-05-08 16:20:55 -0700

    safe paths

--- boot/cli/helpers/maybeSyncExisting.ts
+++ boot/cli/helpers/maybeSyncExisting.ts
@@ -2,15 +2,12 @@ import * as fs from 'fs/promises'
 import * as path from 'path'
 import sspawn from '../../../src/helpers/sspawn'
 import yarncmdRunByAppConsumer from './yarncmdRunByAppConsumer'
-import { loadDreamYamlFile } from '../../../src/helpers/path'
-import absoluteFilePath from '../../../src/helpers/absoluteFilePath'
 
 export default async function maybeSyncExisting(programArgs: string[]) {
-  const yamlConf = await loadDreamYamlFile()
   try {
     const pathToCheck = programArgs.includes('--core')
-      ? process.cwd() + '/src/sync/schema.ts'
-      : process.cwd() + '/../../node_modules/dream/src/sync/schema.ts'
+      ? path.join(process.cwd(), '/src/sync/schema.ts')
+      : path.join(process.cwd(), '/../../node_modules/dream/src/sync/schema.ts')
     await fs.statfs(pathToCheck)
   } catch (_) {
     console.log('Missing schema file, resyncing app')
