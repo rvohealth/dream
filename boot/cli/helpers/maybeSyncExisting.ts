f3aeb2ab6266ec02d830feeb06505017098e012e
avocadojesus <jgarbey@gmail.com>
2023-05-08 16:15:51 -0700

    add debug

--- boot/cli/helpers/maybeSyncExisting.ts
+++ boot/cli/helpers/maybeSyncExisting.ts
@@ -8,6 +8,7 @@ import absoluteFilePath from '../../../src/helpers/absoluteFilePath'
 export default async function maybeSyncExisting(programArgs: string[]) {
   const yamlConf = await loadDreamYamlFile()
   try {
+    console.log('DEBUGGING', absoluteFilePath(yamlConf.schema_path))
     await fs.statfs(absoluteFilePath(yamlConf.schema_path))
   } catch (_) {
     console.log('Missing schema file, resyncing app')
