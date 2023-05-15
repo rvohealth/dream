213a4f5dc83b2dbd3a156eeea55ad2e2ac602cec
avocadojesus <jgarbey@gmail.com>
2023-05-15 12:58:41 -0700

    add generateSerializerContent helper

---
+++ src/helpers/cli/generateSerializerContent.ts
@@ -0,0 +1,58 @@
+export default async function generateSerializerContent(
+  serializerClassName: string,
+  fullyQualifiedModelName: string,
+  attributes: string[] = []
+) {
+  if (!attributes.length)
+    return `\
+import { DreamSerializer } from 'dream'
+
+export default class ${serializerClassName} extends DreamSerializer {
+}`
+  const luxonImport = hasDateTimeType(attributes) ? "import { DateTime } from 'luxon'\n" : ''
+  return `\
+${luxonImport}import { DreamSerializer, Attribute } from 'dream'
+
+export default class ${serializerClassName} extends DreamSerializer {
+  ${attributes
+    .map(attr => {
+      const [name, type] = attr.split(':')
+      return `@Attribute(${attributeSpecifier(type)})
+  public ${name}: ${jsType(type)}`
+    })
+    .join(',\n  ')}
+}\
+`
+}
+
+function attributeSpecifier(type: string) {
+  switch (type) {
+    case 'date':
+      return "'date'"
+    default:
+      return ''
+  }
+}
+
+function jsType(type?: string) {
+  switch (type) {
+    case 'datetime':
+    case 'date':
+      return 'DateTime'
+    case 'decimal':
+    case 'integer':
+    case 'number':
+      return 'number'
+
+    case 'string':
+    case 'text':
+      return 'string'
+
+    default:
+      return 'any'
+  }
+}
+
+function hasDateTimeType(attributes: string[]) {
+  return !!attributes.map(attr => jsType(attr.split(':')[1])).find(a => a === 'DateTime')
+}
