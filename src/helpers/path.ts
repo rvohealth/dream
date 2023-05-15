d95741f5809e311264c312f50745c6ed6d529423
avocadojesus <jgarbey@gmail.com>
2023-05-15 12:59:13 -0700

    add serializers_path to dream yaml schema

--- src/helpers/path.ts
+++ src/helpers/path.ts
@@ -76,6 +76,7 @@ export async function dreamsConfigPath({ omitDirname }: { omitDirname?: boolean 
 
 export interface DreamYamlFile {
   models_path: string
+  serializers_path: string
   associations_path: string
   migrations_path: string
   schema_path: string
