5304fe5d298767ecd71c49e5822927c2720c6b39
avocadojesus <jgarbey@gmail.com>
2023-06-24 13:09:10 -0700

    move posts migration after post_visibilities migration

--- test-app/db/migrations/1682968931719-create-posts.ts
+++ test-app/db/migrations/1682968931719-create-posts.ts
@@ -5,7 +5,9 @@ export async function up(db: Kysely<any>): Promise<void> {
     .createTable('posts')
     .addColumn('id', 'bigserial', col => col.primaryKey())
     .addColumn('user_id', 'bigint', col => col.references('users.id').onDelete('cascade').notNull())
-    .addColumn('post_visibility_id', 'bigint', col => col.references('posts.id').onDelete('cascade'))
+    .addColumn('post_visibility_id', 'bigint', col =>
+      col.references('post_visibilities.id').onDelete('cascade')
+    )
     .addColumn('body', 'text')
     .addColumn('created_at', 'timestamp', col => col.notNull())
     .addColumn('updated_at', 'timestamp', col => col.notNull())
