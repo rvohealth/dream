bf5e6e107d7b573fe252dae64ef738db8b500447
avocadojesus <jgarbey@gmail.com>
2023-07-10 23:46:10 -0700

    add originalData accessor

--- src/serializer/index.ts
+++ src/serializer/index.ts
@@ -38,6 +38,10 @@ export default class DreamSerializer {
     return { ...this._data }
   }
 
+  public get originalData() {
+    return this._data
+  }
+
   public get attributes() {
     const attributes = [...(this.constructor as typeof DreamSerializer).attributeStatements.map(s => s.field)]
 
