a6470a0f090fb8fb485bd45d461cc0299f1a31b8
Daniel Nelson <844258+daniel-nelson@users.noreply.github.com>
2023-08-03 11:06:46 -0500

    Declaration files generatable Build docs doesn't include test-app folder Built docs don't freeze browser

--- boot/build-docs.ts
+++ boot/build-docs.ts
@@ -4,7 +4,7 @@ import pack from '../package.json'
 
 export default async function buildDocs() {
   console.log('generating docs for dream version: ' + pack.version + '...')
-  await sspawn(`yarn typedoc src/index.ts --out docs/${pack.version}`)
+  await sspawn(`yarn typedoc src/index.ts --tsconfig ./tsconfig.build.json --out docs/${pack.version}`)
   console.log('done!')
 }
 buildDocs()
