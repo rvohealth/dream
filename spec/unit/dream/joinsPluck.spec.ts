e035169ddc06e18e9143f7b800e6075ce8596c36
Daniel Nelson <844258+daniel-nelson@users.noreply.github.com>
2023-08-08 15:24:24 -0500

    Spec to make sure can follow where clause with multiple association names in joinsPluck

--- spec/unit/dream/joinsPluck.spec.ts
+++ spec/unit/dream/joinsPluck.spec.ts
@@ -1,3 +1,7 @@
+import User from '../../../test-app/app/models/User'
+import Composition from '../../../test-app/app/models/Composition'
+import CompositionAsset from '../../../test-app/app/models/CompositionAsset'
+import CompositionAssetAudit from '../../../test-app/app/models/CompositionAssetAudit'
 import Node from '../../../test-app/app/models/Graph/Node'
 import Edge from '../../../test-app/app/models/Graph/Edge'
 import EdgeNode from '../../../test-app/app/models/Graph/EdgeNode'
@@ -41,17 +45,31 @@ describe('Query#joinsPluck', () => {
     expect(plucked).toEqual([[edge1.id, edge1.name]])
   })
 
-  it('association name after conditional', async () => {
-    const node = await Node.create({ name: 'N1' })
-    const edge1 = await Edge.create({ name: 'E1' })
-    const edge2 = await Edge.create({ name: 'E2' })
-    await EdgeNode.create({ node, edge: edge1 })
-    await EdgeNode.create({ node, edge: edge2 })
+  it('two association names after conditional', async () => {
+    const user = await User.create({ email: 'fred@frewd', password: 'howyadoin' })
 
-    const plucked = await node.joinsPluck('edgeNodes', { edge_id: edge2.id }, 'edge', [
-      'edge.id',
-      'edge.name',
-    ])
-    expect(plucked).toEqual([[edge2.id, edge2.name]])
+    const composition1 = await Composition.create({ user })
+    const compositionAsset1 = await CompositionAsset.create({ composition: composition1 })
+    const compositionAssetAudit1 = await CompositionAssetAudit.create({
+      compositionAsset: compositionAsset1,
+      approval: true,
+    })
+
+    const composition2 = await Composition.create({ user })
+    const compositionAsset2 = await CompositionAsset.create({ composition: composition2 })
+    const compositionAssetAudit2 = await CompositionAssetAudit.create({
+      compositionAsset: compositionAsset2,
+      approval: true,
+    })
+
+    const plucked = await user.joinsPluck(
+      'compositions',
+      { id: composition2.id },
+      'compositionAssets',
+      'compositionAssetAudits',
+      ['compositionAssetAudits.id', 'compositionAssetAudits.approval']
+    )
+
+    expect(plucked).toEqual([[compositionAssetAudit2.id, true]])
   })
 })
