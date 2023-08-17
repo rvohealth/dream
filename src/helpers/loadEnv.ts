bad3ebe440641e768799294b40cd35f515ab98e7
avocadojesus <jgarbey@gmail.com>
2023-08-17 00:24:25 -0700

    add debug

--- src/helpers/loadEnv.ts
+++ src/helpers/loadEnv.ts
@@ -6,11 +6,12 @@ if (process.env.DREAM_CORE_DEVELOPMENT === '1') {
   console.log('DEBUG 1', __dirname)
   dotenv.config({ path: dotenvpath })
 } else {
-  console.log('DEBUG 2', __dirname)
+  const dotenvpath =
+    process.env.NODE_ENV === 'test'
+      ? __dirname + '../../../../../.env.test'
+      : __dirname + '../../../../../.env'
+  console.log('DEBUG 2', __dirname, dotenvpath)
   dotenv.config({
-    path:
-      process.env.NODE_ENV === 'test'
-        ? __dirname + '../../../../../.env.test'
-        : __dirname + '../../../../../.env',
+    path: dotenvpath,
   })
 }
