f06a383b3c5518e75b844cfe742a194b7000eecb
avocadojesus <jgarbey@gmail.com>
2023-08-17 00:13:07 -0700

    another debug

--- src/helpers/loadEnv.ts
+++ src/helpers/loadEnv.ts
@@ -3,6 +3,7 @@ import dotenv from 'dotenv'
 if (process.env.DREAM_CORE_DEVELOPMENT === '1') {
   const dotenvpath =
     process.env.NODE_ENV === 'test' ? __dirname + '/../../.env.test' : __dirname + '/../../../.env'
+  console.log('DEBUG 1', __dirname)
   dotenv.config({ path: dotenvpath })
 } else {
   console.log('DEBUG 2', __dirname)
