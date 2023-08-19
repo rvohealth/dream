e9c4bfc7c22d522d132c4602e1588b4e4832c898
avocadojesus <jgarbey@gmail.com>
2023-08-19 07:01:59 -0700

    dream cmds pass node env

--- boot/cli/helpers/nodeOrTsnodeCmd.ts
+++ boot/cli/helpers/nodeOrTsnodeCmd.ts
@@ -8,12 +8,14 @@ export default function nodeOrTsnodeCmd(
   { nodeFlags = [], fileArgs = [] }: { nodeFlags?: string[]; fileArgs?: string[] } = {}
 ) {
   const coreDevFlag = setCoreDevelopmentFlag(programArgs)
-  const useTsnode = programArgs.includes('--tsnode')
+  const useTsnode = programArgs.includes('--tsnode') || process.env.TS_SAFE === '1'
   const nodeCmd = useTsnode ? 'npx ts-node' : 'node'
   const omitDistFromPathEnv = useTsnode ? 'DREAM_OMIT_DIST_FOLDER=1 ' : ''
   const realFilePath = useTsnode ? filePath : path.join('dist', filePath.replace(/\.ts$/, '.js'))
   if (useTsnode) fileArgs.push('--tsnode')
-  return `${coreDevFlag}${omitDistFromPathEnv}${nodeCmd} ${
+  const nodeEnvFlag = process.env.NODE_ENV ? `NODE_ENV=${process.env.NODE_ENV} ` : ''
+
+  return `${nodeEnvFlag}${coreDevFlag}${omitDistFromPathEnv}${nodeCmd} ${
     useTsnode ? '' : nodeFlags.join(' ')
   } ${realFilePath} ${fileArgs.join(' ')} `
 }
