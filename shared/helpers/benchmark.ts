e8f5967d310676e7fe58cbfd728fa0750757fe16
avocadojesus <jgarbey@gmail.com>
2023-08-26 00:57:17 -0700

    update benchmarking

--- shared/helpers/benchmark.ts
+++ shared/helpers/benchmark.ts
@@ -4,13 +4,19 @@ export type BenchmarkLogLevel = 'log' | 'warn' | 'error'
 
 export default class Benchmark {
   private _start: DateTime
+  private _startdate: Date
   public start() {
     this._start = DateTime.now()
+    this._startdate = new Date()
   }
 
   public mark(message: string, level: BenchmarkLogLevel = 'log') {
     if (process.env.NODE_ENV === 'test' && process.env.ALLOW_BENCHMARKS !== '1') return
     if (!this._start) this.start()
-    console[level](message, DateTime.now().diff(this._start, 'milliseconds').milliseconds)
+    console[level](
+      message,
+      DateTime.now().diff(this._start, 'milliseconds').milliseconds,
+      `(alternate benchmark: ${new Date().getMilliseconds() - this._startdate.getMilliseconds()})`
+    )
   }
 }
