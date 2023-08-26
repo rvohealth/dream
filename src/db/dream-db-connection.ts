f5d59fa75a006e947c13064f0b5be3e5c23fc11f
avocadojesus <jgarbey@gmail.com>
2023-08-26 15:28:52 -0700

    remove internal benchmarks

--- src/db/dream-db-connection.ts
+++ src/db/dream-db-connection.ts
@@ -3,8 +3,6 @@ import { DB } from '../sync/schema'
 import ConnectionConfRetriever from './connection-conf-retriever'
 import { DbConnectionType } from './types'
 import { Pool } from 'pg'
-import Benchmark from '../../shared/helpers/benchmark'
-import debug from '../../shared/helpers/debug'
 
 const connections = {} as any
 
@@ -15,9 +13,6 @@ export default class DreamDbConnection {
 
     const connectionConf = new ConnectionConfRetriever().getConnectionConf(connectionType)
 
-    const benchmark = new Benchmark()
-    benchmark.start()
-    benchmark.mark('BEGINNING CONNECT TO DB...')
     const pool = new Pool({
       user: process.env[connectionConf.user] || '',
       password: process.env[connectionConf.password] || '',
@@ -25,21 +20,11 @@ export default class DreamDbConnection {
       host: process.env[connectionConf.host] || 'localhost',
       port: process.env[connectionConf.port] ? parseInt(process.env[connectionConf.port]!) : 5432,
       ssl: connectionConf.use_ssl ? process.env[connectionConf.use_ssl] === '1' : false,
-      allowExitOnIdle: true,
-      connectionTimeoutMillis: 7000,
-      // statement_timeout: 60000,
-      // query_timeout: 60000,
-      // max: process.env.MAX_DB_CONNECTIONS ? parseInt(process.env.MAX_DB_CONNECTIONS!) : 10,
-      // idleTimeoutMillis: 60 * 60 * 24 * 1000,
     })
 
     const dbConn = new Kysely<DB>({
       dialect: new PostgresDialect({
         pool,
-        onCreateConnection: async () => {
-          console.log('NUMBER OF DB CONNECTIONS: ', pool.totalCount)
-          benchmark.mark('ENDING CONNECT TO DB...')
-        },
       }),
     })
 
