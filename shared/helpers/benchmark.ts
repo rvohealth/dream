932cc97537b44a780096b0c1943e6c41b36c2966
avocadojesus <jgarbey@gmail.com>
2023-08-26 15:09:11 -0700

    remove invalid date comparison (next time use .getTime instead of .getMilliseconds)

--- shared/helpers/benchmark.ts
+++ shared/helpers/benchmark.ts
@@ -4,21 +4,13 @@ export type BenchmarkLogLevel = 'log' | 'warn' | 'error'
 
 export default class Benchmark {
   private _start: DateTime
-  private _startdate: Date
   public start() {
     this._start = DateTime.now()
-    this._startdate = new Date()
   }
 
   public mark(message: string, level: BenchmarkLogLevel = 'log') {
     if (process.env.NODE_ENV === 'test' && process.env.ALLOW_BENCHMARKS !== '1') return
     if (!this._start) this.start()
-    console[level](
-      message,
-      `(alternate benchmark1: ${new Date().getMilliseconds() - this._startdate.getMilliseconds()})`,
-      DateTime.now().diff(this._start, 'milliseconds').milliseconds,
-      `(alternate benchmark2: ${new Date().getMilliseconds() - this._startdate.getMilliseconds()})`,
-      DateTime.now().diff(this._start, 'milliseconds').milliseconds
-    )
+    console[level](message, DateTime.now().diff(this._start, 'milliseconds').milliseconds)
   }
 }
