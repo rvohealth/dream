6d124efd9bf40f2dca967fe6380890c7db3a1fc0
avocadojesus <jgarbey@gmail.com>
2023-08-30 09:59:09 -0700

    always write schema and associations files

--- boot/sync-existing-or-create-boilerplate.ts
+++ boot/sync-existing-or-create-boilerplate.ts
@@ -21,29 +21,21 @@ export default async function syncExistingOrCreateBoilerplate() {
   }
 
   try {
-    await fs.statfs('./src/sync/schema.ts')
+    await fs.statfs(`../../${yamlConf.schema_path}`)
+    console.log('existing schema file found within project, copying to sync dir...')
+    await sspawn(`cp ../../${yamlConf.schema_path} ./src/sync/schema.ts`)
   } catch (_) {
-    try {
-      await fs.statfs(`../../${yamlConf.schema_path}`)
-      console.log('existing schema file found within project, copying to sync dir...')
-      await sspawn(`cp ../../${yamlConf.schema_path} ./src/sync/schema.ts`)
-    } catch (_) {
-      console.log('schema file missing, adding boilerplate schema file')
-      await sspawn('cp ./boot/boilerplate/sync/schema.ts ./src/sync/schema.ts')
-    }
+    console.log('schema file missing, adding boilerplate schema file')
+    await sspawn('cp ./boot/boilerplate/sync/schema.ts ./src/sync/schema.ts')
   }
 
   try {
-    await fs.statfs('./src/sync/associations.ts')
+    await fs.statfs(`../../${yamlConf.associations_path}`)
+    console.log('existing associations file found within project, copying to sync dir...')
+    await sspawn(`cp ../../${yamlConf.associations_path} ./src/sync/associations.ts`)
   } catch (_) {
-    try {
-      await fs.statfs(`../../${yamlConf.associations_path}`)
-      console.log('existing associations file found within project, copying to sync dir...')
-      await sspawn(`cp ../../${yamlConf.associations_path} ./src/sync/associations.ts`)
-    } catch (_) {
-      console.log('associations file missing, adding boilerplate dream file')
-      await sspawn('cp ./boot/boilerplate/sync/associations.ts ./src/sync/associations.ts')
-    }
+    console.log('associations file missing, adding boilerplate dream file')
+    await sspawn('cp ./boot/boilerplate/sync/associations.ts ./src/sync/associations.ts')
   }
 }
 
