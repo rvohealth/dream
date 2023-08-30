2011fb89413332ca27fdb070f55932bb1fc3ae8a
avocadojesus <jgarbey@gmail.com>
2023-08-30 09:56:05 -0700

    fix build path issues in core dev causing incorrect file to generate

--- boot/build-config-cache.ts
+++ boot/build-config-cache.ts
@@ -2,7 +2,9 @@ import './cli/helpers/loadAppEnvFromBoot'
 import path from 'path'
 import { promises as fs } from 'fs'
 import compact from '../shared/helpers/compact'
+import dreamRootPath from '../shared/helpers/path/dreamRootPath'
 import loadDbConfigYamlFile from '../shared/helpers/path/loadDbConfigYamlFile'
+import projectRootPath from '../shared/helpers/path/projectRootPath'
 
 export default async function buildConfigCache() {
   const dbConf = await loadDbConfigYamlFile()
@@ -14,19 +16,8 @@ export default {
 }
   `
 
-  let distSyncPathParts = compact([
-    __dirname,
-    process.env.DREAM_OMIT_DIST_FOLDER === '1' ? null : '..',
-    'src',
-    'sync',
-  ])
-  let rootSyncPathParts = compact([
-    __dirname,
-    '..',
-    process.env.DREAM_OMIT_DIST_FOLDER === '1' ? null : '..',
-    'src',
-    'sync',
-  ])
+  let distSyncPathParts = compact([dreamRootPath(), 'dist', 'src', 'sync'])
+  let rootSyncPathParts = compact([dreamRootPath(), 'src', 'sync'])
   const filePath = path.join(...distSyncPathParts, 'config-cache.ts')
   const originalFilePath = path.join(...rootSyncPathParts, 'config-cache.ts')
 
