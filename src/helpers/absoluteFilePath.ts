df3687fe2b25c8a7c83cd052d1c39401cfc08dc5
avocadojesus <jgarbey@gmail.com>
2023-08-31 09:59:39 -0700

    fix core development path issue for generating models

--- src/helpers/absoluteFilePath.ts
+++ src/helpers/absoluteFilePath.ts
@@ -3,7 +3,12 @@
 // would be the most stable moving forward, especially if we ever decide to
 // build to dist, since directory structures morph in those contexts.
 import path from 'path'
+import dreamOrPsychicCoreDevelopment from '../../shared/helpers/dreamOrPsychicCoreDevelopment'
 
 export default function absoluteFilePath(filePath: string) {
+  if (dreamOrPsychicCoreDevelopment()) {
+    filePath = filePath.replace(/^[/]?test-app/, '')
+  }
+
   return path.join(process.env.APP_ROOT_PATH!, filePath)
 }
