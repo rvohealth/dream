cb571faffd7f296891dd39b1c98bb6f76e1da560
avocadojesus <jgarbey@gmail.com>
2023-10-03 12:04:15 -0700

    use @rvohealth namespace when pointing to dream from node modules

--- boot/cli/helpers/dreamjsOrDreamtsCmd.ts
+++ boot/cli/helpers/dreamjsOrDreamtsCmd.ts
@@ -10,10 +10,10 @@ export default function dreamjsOrDreamtsCmd(
   const useTsnode = programArgs.includes('--tsnode') || process.env.TS_SAFE === '1'
   const dreamCmd = useTsnode ? 'dreamts' : 'dreamjs'
   const omitDistFromPathEnv = useTsnode ? 'DREAM_OMIT_DIST_FOLDER=1 ' : ''
-  const basepath = process.env.DREAM_CORE_DEVELOPMENT === '1' ? '' : '../../'
+  const basepath = process.env.DREAM_CORE_DEVELOPMENT === '1' ? '' : '../../../'
   if (useTsnode) cmdArgs.push('--tsnode')
 
-  const fullcmd = `${coreDevFlag}${omitDistFromPathEnv}yarn --cwd=${basepath}node_modules/dream ${dreamCmd} ${cmd} ${cmdArgs.join(
+  const fullcmd = `${coreDevFlag}${omitDistFromPathEnv}yarn --cwd=${basepath}node_modules/@rvohealth/dream ${dreamCmd} ${cmd} ${cmdArgs.join(
     ' '
   )} `
 
