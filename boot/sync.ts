384acf21f8708c84c30cef262386043267bacd9f
avocadojesus <jgarbey@gmail.com>
2023-10-03 12:13:59 -0700

    fix updirs for sync

--- boot/sync.ts
+++ boot/sync.ts
@@ -21,9 +21,16 @@ async function writeSchema() {
   const yamlConf = await loadDreamYamlFile()
   const dbConf = await new ConnectionConfRetriever().getConnectionConf('primary')
 
-  const updirsToDreamRoot = shouldOmitDistFolder() ? ['..'] : ['..', '..', '..']
+  const updirsToDreamRoot = shouldOmitDistFolder() ? ['..'] : ['..', '..']
   let absoluteSchemaPath = path.join(__dirname, ...updirsToDreamRoot, yamlConf.schema_path)
-  let absoluteSchemaWritePath = path.join(__dirname, ...updirsToDreamRoot, '..', '..', yamlConf.schema_path)
+  let absoluteSchemaWritePath = path.join(
+    __dirname,
+    ...updirsToDreamRoot,
+    '..',
+    '..',
+    '..',
+    yamlConf.schema_path
+  )
   if (process.env.DREAM_CORE_DEVELOPMENT === '1') {
     absoluteSchemaWritePath = path.join(__dirname, '..', yamlConf.schema_path)
     absoluteSchemaPath = path.join(__dirname, '..', yamlConf.schema_path)
