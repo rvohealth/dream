67c4921bbd7866a75ddaf6d713bb4dda0907a398
avocadojesus <jgarbey@gmail.com>
2023-10-06 08:19:56 -0700

    only resort if necessary

---
+++ spec/unit/dream/decorators/helpers/isSortedCorrectly.spec.ts
@@ -0,0 +1,47 @@
+import isSortedCorrectly from '../../../../../src/decorators/sortable/helpers/isSortedCorrectly'
+
+describe('isSortedCorrectly', () => {
+  let subject = () => isSortedCorrectly(data, 'position')
+  let data: any[]
+
+  context('with valid positions passed', () => {
+    beforeEach(() => {
+      data = [
+        { position: 1 },
+        { position: 2 },
+        { position: 3 },
+        { position: 4 },
+        { position: 5 },
+        { position: 6 },
+        { position: 7 },
+        { position: 8 },
+        { position: 9 },
+        { position: 10 },
+      ]
+    })
+    it('returns true', () => {
+      expect(subject()).toBe(true)
+    })
+  })
+
+  context('with invalid positions passed', () => {
+    beforeEach(() => {
+      data = [
+        { position: 10 },
+        { position: 1 },
+        { position: 2 },
+        { position: 3 },
+        { position: 4 },
+        { position: 5 },
+        { position: 6 },
+        { position: 7 },
+        { position: 8 },
+        { position: 9 },
+      ]
+    })
+
+    it('returns false', () => {
+      expect(subject()).toBe(false)
+    })
+  })
+})
