4b8ae7503978dcf2f8476d6a5a3da552103d648a
avocadojesus <jgarbey@gmail.com>
2023-12-22 15:42:31 -0800

    cleanup

--- src/dream/internal/associations/destroyAssociation.ts
+++ src/dream/internal/associations/destroyAssociation.ts
@@ -1,9 +1,6 @@
 import Dream from '../../../dream'
 import DreamTransaction from '../../transaction'
 import { UpdateablePropertiesForClass } from '../../types'
-import { HasManyStatement } from '../../../decorators/associations/has-many'
-import { HasOneStatement } from '../../../decorators/associations/has-one'
-import CannotDestroyAssociationWithThroughContext from '../../../exceptions/associations/cannot-destroy-association-with-through-context'
 import associationUpdateQuery from './associationUpdateQuery'
 
 export default async function destroyAssociation<
@@ -20,18 +17,6 @@ export default async function destroyAssociation<
   associationName: AssociationName,
   opts: UpdateablePropertiesForClass<AssociationType & typeof Dream> = {}
 ): Promise<number> {
-  const association = dream.associationMap()[associationName] as
-    | HasManyStatement<any, any, any>
-    | HasOneStatement<any, any, any>
-
-  const associationClass = association.modelCB()
-
-  if (Array.isArray(associationClass)) {
-    throw new Error(`
-      Cannot destroy polymorphic associations using destroyAssociation
-    `)
-  }
-
   const query = associationUpdateQuery(dream, txn, associationName)
   return await query.where(opts as any).destroy()
 }
