e66b51838ee79bb1235b8c85246d5fa34d862c54
avocadojesus <jgarbey@gmail.com>
2023-12-22 15:11:40 -0800

    add support for destroying through associations

--- src/dream/internal/associations/associationUpdateQuery.ts
+++ src/dream/internal/associations/associationUpdateQuery.ts
@@ -1,9 +1,10 @@
 import Dream from '../../../dream'
 import DreamTransaction from '../../transaction'
 import Query from '../../query'
-import { DreamConstructorType, TableOrAssociationName } from '../../types'
+import { DreamConstructorType, TableOrAssociationName, UpdateablePropertiesForClass } from '../../types'
 import { HasManyStatement } from '../../../decorators/associations/has-many'
 import { HasOneStatement } from '../../../decorators/associations/has-one'
+import applyOrderAndLimitToAssociationQuery from './applyOrderAndLimitToAssociationQuery'
 
 export default function associationUpdateQuery<
   DreamInstance extends Dream,
@@ -26,7 +27,6 @@ export default function associationUpdateQuery<
   const associationClass = association.modelCB()
 
   const dreamClass = dream.constructor as typeof Dream
-  const dreamClassOrTransaction = (txn ? dreamClass.txn(txn) : dreamClass) as any
 
   const nestedScope = txn
     ? dreamClass.txn(txn).joins(association.as as any)
@@ -35,27 +35,12 @@ export default function associationUpdateQuery<
   const nestedSelect = nestedScope
     // @ts-ignore
     .where({ [dream.primaryKey]: dream.primaryKeyValue })
-    .nestedSelect(`${association.as}.${associationClass.primaryKey}` as any)
+    .nestedSelect(`${association.as}.${associationClass.primaryKey}`)
 
   const whereClause = {
     [associationClass.primaryKey]: nestedSelect,
   }
 
-  let query = txn
-    ? (associationClass.txn(txn).where(whereClause) as any)
-    : (associationClass.where(whereClause) as any)
-
-  if (association.order) {
-    if (Array.isArray(association.order)) {
-      query = query.order(association.order[0], association.order[1])
-    } else {
-      query = query.order(association.order, 'asc')
-    }
-  }
-
-  if (association.type === 'HasOne') {
-    query = query.limit(1)
-  }
-
+  const query = txn ? associationClass.txn(txn).where(whereClause) : associationClass.where(whereClause)
   return query as AssociationQuery
 }
--- src/dream/internal/associations/destroyAssociation.ts
+++ src/dream/internal/associations/destroyAssociation.ts
@@ -1,10 +1,10 @@
 import Dream from '../../../dream'
 import DreamTransaction from '../../transaction'
-import { DreamConstructorType, UpdateablePropertiesForClass } from '../../types'
+import { UpdateablePropertiesForClass } from '../../types'
 import { HasManyStatement } from '../../../decorators/associations/has-many'
 import { HasOneStatement } from '../../../decorators/associations/has-one'
-import { BelongsToStatement } from '../../../decorators/associations/belongs-to'
 import CannotDestroyAssociationWithThroughContext from '../../../exceptions/associations/cannot-destroy-association-with-through-context'
+import associationUpdateQuery from './associationUpdateQuery'
 
 export default async function destroyAssociation<
   DreamInstance extends Dream,
@@ -23,54 +23,15 @@ export default async function destroyAssociation<
   const association = dream.associationMap()[associationName] as
     | HasManyStatement<any, any, any>
     | HasOneStatement<any, any, any>
-    | BelongsToStatement<any, any, any>
 
-  if (Array.isArray(association.modelCB())) {
+  const associationClass = association.modelCB()
+
+  if (Array.isArray(associationClass)) {
     throw new Error(`
       Cannot destroy polymorphic associations using destroyAssociation
     `)
   }
-  const associationClass = association.modelCB() as typeof Dream
 
-  switch (association.type) {
-    case 'HasMany':
-    case 'HasOne':
-      if ((association as HasManyStatement<any, any, any>).through)
-        throw new CannotDestroyAssociationWithThroughContext({
-          dreamClass: dream.constructor as typeof Dream,
-          association,
-        })
-
-      let query = associationClass
-        // NOTE: do not remove this ts-ignore. It only breaks when other apps
-        // load their schemas in.
-        // @ts-ignore
-        .where({
-          [association.foreignKey()]: dream.primaryKeyValue,
-          ...opts,
-        })
-
-      if (association.type === 'HasOne') {
-        query = query.limit(1)
-      }
-
-      if (association.order) {
-        if (Array.isArray(association.order)) {
-          query = query.order(association.order[0], association.order[1])
-        } else {
-          query = query.order(association.order, 'asc')
-        }
-      }
-
-      return await query.destroy()
-
-    case 'BelongsTo':
-      // NOTE: dream relies on the database being properly set up with cascade deletion on the foreign key.
-      // Our dream generators automatically handle dream in the migration layer by setting col.onDelete('cascade')
-      return await (associationClass as any)
-        .where({
-          [associationClass.primaryKey]: (dream as any)[association.foreignKey()],
-        })
-        .destroy()
-  }
+  const query = associationUpdateQuery(dream, txn, associationName)
+  return await query.where(opts as any).destroy()
 }
