d99ec3ef6373060ce9f98d39675762b09a4fad9c
avocadojesus <jgarbey@gmail.com>
2024-01-11 13:04:32 -0800

    ensure that if a string is passed to either a date or datetime, we convert it to a Luxon DateTime when setting attributes
    related https://rvohealth.atlassian.net/browse/PDTC-3946

---
+++ src/helpers/db/types/isDateTime.ts
@@ -0,0 +1,10 @@
+import Dream from '../../../dream'
+
+export default function isDateTime<
+  T extends typeof Dream,
+  DB extends InstanceType<T>['DB'],
+  TableName extends keyof DB = InstanceType<T>['table'] & keyof DB,
+  Table extends DB[keyof DB] = DB[TableName]
+>(dreamClass: T, column: keyof Table): boolean {
+  return ['timestamp without time zone', 'timestamp'].includes(dreamClass.cachedTypeFor(column))
+}
