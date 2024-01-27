3ea2521d01f38a2fec2fac6a5e66e80e94c9c045
avocadojesus <jgarbey@gmail.com>
2024-01-27 13:48:10 -0800

    reduce type complexity - sqlResultToDreamInstance

--- src/dream/internal/sqlResultToDreamInstance.ts
+++ src/dream/internal/sqlResultToDreamInstance.ts
@@ -3,37 +3,44 @@ import { AssociationTableNames } from '../../db/reflections'
 import Dream from '../../dream'
 import STIChildMissing from '../../exceptions/sti/sti-child-missing'
 
-export default function sqlResultToDreamInstance<
-  DreamClass extends typeof Dream,
-  DreamInstance extends InstanceType<DreamClass> = InstanceType<DreamClass>,
-  SyncedAssociations extends DreamInstance['syncedAssociations'] = DreamInstance['syncedAssociations'],
-  DB extends DreamInstance['DB'] = DreamInstance['DB'],
-  TableName extends AssociationTableNames<DB, SyncedAssociations> &
-    keyof DB = InstanceType<DreamClass>['table'],
-  Table extends DB[TableName] = DB[TableName]
->(dreamClass: DreamClass, sqlResult: any): InstanceType<DreamClass> | Dream {
+// original function types:
+// NOTE: these types were intenionally removed to reduce type complexity.
+// if you are working on this method, and would like the assistance of the type
+// helpers, feel free to uncomment this and use them to aid in improving this function.
+// however, since this function is internal, we are leaving the types to reduce complexity.
+//
+// export default function sqlResultToDreamInstance<
+//   DreamClass extends typeof Dream,
+//   DreamInstance extends InstanceType<DreamClass> = InstanceType<DreamClass>,
+//   SyncedAssociations extends DreamInstance['syncedAssociations'] = DreamInstance['syncedAssociations'],
+//   DB extends DreamInstance['DB'] = DreamInstance['DB'],
+//   TableName extends AssociationTableNames<DB, SyncedAssociations> &
+//     keyof DB = InstanceType<DreamClass>['table'],
+//   Table extends DB[TableName] = DB[TableName]
+// >(dreamClass: DreamClass, sqlResult: any): InstanceType<DreamClass> | Dream {
+export default function sqlResultToDreamInstance(dreamClass: any, sqlResult: any): any {
   if (dreamClass['isSTIBase']) {
     const extendingDreamClass = findExtendingDreamClass(dreamClass, sqlResult.type)
 
     if (!extendingDreamClass)
       throw new STIChildMissing(dreamClass, sqlResult.type, sqlResult[dreamClass.primaryKey])
 
-    return extendingDreamClass.new(sqlResult as Updateable<Table>)
+    return extendingDreamClass.new(sqlResult)
   } else {
-    return dreamClass.new(sqlResult as Updateable<Table>)
+    return dreamClass.new(sqlResult)
   }
 }
 
 export function findExtendingDreamClass(dreamClass: typeof Dream, type: string): typeof Dream | undefined {
-  if (!dreamClass['extendedBy']) return undefined
+  if (!(dreamClass as any)['extendedBy']) return undefined
 
-  const extendingDreamClass = dreamClass['extendedBy'].find(
-    extendingDreamClass => extendingDreamClass.name === type
+  const extendingDreamClass = (dreamClass as any)['extendedBy'].find(
+    (extendingDreamClass: any) => extendingDreamClass.name === type
   )
 
   if (extendingDreamClass) return extendingDreamClass
 
-  return dreamClass['extendedBy']
-    .map(extendingDreamClass => findExtendingDreamClass(extendingDreamClass, type))
-    .find(dreamClassOrUndefined => dreamClassOrUndefined)
+  return (dreamClass as any)['extendedBy']
+    .map((extendingDreamClass: any) => findExtendingDreamClass(extendingDreamClass, type))
+    .find((dreamClassOrUndefined: any) => dreamClassOrUndefined)
 }
