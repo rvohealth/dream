738e7db7aa292a464d0353b730065f0e748ea9ad
avocadojesus <jgarbey@gmail.com>
2024-01-27 16:06:51 -0800

    reduce type complexity for destroyAssociation internal helper

--- src/dream/internal/associations/destroyAssociation.ts
+++ src/dream/internal/associations/destroyAssociation.ts
@@ -3,19 +3,26 @@ import DreamTransaction from '../../transaction'
 import { UpdateablePropertiesForClass } from '../../types'
 import associationUpdateQuery from './associationUpdateQuery'
 
-export default async function destroyAssociation<
-  DreamInstance extends Dream,
-  SyncedAssociations extends DreamInstance['syncedAssociations'],
-  AssociationName extends keyof SyncedAssociations[DreamInstance['table']],
-  PossibleArrayAssociationType = DreamInstance[AssociationName & keyof DreamInstance],
-  AssociationType = PossibleArrayAssociationType extends (infer ElementType)[]
-    ? ElementType
-    : PossibleArrayAssociationType
->(
-  dream: DreamInstance,
-  txn: DreamTransaction<DreamInstance['DB']> | null = null,
-  associationName: AssociationName,
-  opts: UpdateablePropertiesForClass<AssociationType & typeof Dream> = {}
+// @reduce-type-complexity
+// export default async function destroyAssociation<
+//   DreamInstance extends Dream,
+//   SyncedAssociations extends DreamInstance['syncedAssociations'],
+//   AssociationName extends keyof SyncedAssociations[DreamInstance['table']],
+//   PossibleArrayAssociationType = DreamInstance[AssociationName & keyof DreamInstance],
+//   AssociationType = PossibleArrayAssociationType extends (infer ElementType)[]
+//     ? ElementType
+//     : PossibleArrayAssociationType
+// >(
+//   dream: DreamInstance,
+//   txn: DreamTransaction<DreamInstance['DB']> | null = null,
+//   associationName: AssociationName,
+//   opts: UpdateablePropertiesForClass<AssociationType & typeof Dream> = {}
+// ): Promise<number> {
+export default async function destroyAssociation(
+  dream: any,
+  txn: any = null,
+  associationName: any,
+  opts: any = {}
 ): Promise<number> {
   const query = associationUpdateQuery(dream, txn, associationName)
   return await query.where(opts as any).destroy()
