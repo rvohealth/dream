2f7bfded80ffb2cd4d2e6bb8f4345d22ec844aec
avocadojesus <jgarbey@gmail.com>
2024-02-02 10:01:33 -0800

    leverage setAttributes

--- src/dream/internal/saveDream.ts
+++ src/dream/internal/saveDream.ts
@@ -50,7 +50,7 @@ export default async function saveDream<DreamInstance extends Dream>(
   }
 
   const data = await executeDatabaseQuery(query.returning(dream.columns()), 'executeTakeFirstOrThrow')
-  dream['_setAttributes'](data, { bypassUserDefinedSetters: true })
+  dream.setAttributes(data)
 
   // set frozen attributes to what has already been saved
   dream['freezeAttributes']()
