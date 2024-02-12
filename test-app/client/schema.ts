a4dc214862740291437c4e2ed2403a589d7d23e5
avocadojesus <jgarbey@gmail.com>
2024-02-12 08:39:47 -0800

    don't format client schema, schema.blank.ts

--- test-app/client/schema.ts
+++ test-app/client/schema.ts
@@ -1,48 +1,47 @@
 export type BalloonColorsEnum = 'blue' | 'green' | 'red'
-export const BalloonColorsEnumArray = ['blue', 'green', 'red']
+export const BalloonColorsEnumArray = [
+  'blue',
+  'green',
+  'red'
+]
 
 export type BalloonLineMaterialsEnum = 'nylon' | 'ribbon' | 'twine' | 'yarn'
-export const BalloonLineMaterialsEnumArray = ['nylon', 'ribbon', 'twine', 'yarn']
+export const BalloonLineMaterialsEnumArray = [
+  'nylon',
+  'ribbon',
+  'twine',
+  'yarn'
+]
 
 export type BalloonTypesEnum = 'Animal' | 'Latex' | 'Mylar'
-export const BalloonTypesEnumArray = ['Animal', 'Latex', 'Mylar']
+export const BalloonTypesEnumArray = [
+  'Animal',
+  'Latex',
+  'Mylar'
+]
 
 export type CatTreats = 'cat-safe chalupas (catlupas,supaloopas)' | 'chicken' | 'ocean fish' | 'tuna'
-export const CatTreatsArray = ['cat-safe chalupas (catlupas,supaloopas)', 'chicken', 'ocean fish', 'tuna']
+export const CatTreatsArray = [
+  'cat-safe chalupas (catlupas,supaloopas)',
+  'chicken',
+  'ocean fish',
+  'tuna'
+]
 
 export type ExtraRateableTypesEnum = 'Balloon' | 'Composition' | 'Post'
-export const ExtraRateableTypesEnumArray = ['Balloon', 'Composition', 'Post']
+export const ExtraRateableTypesEnumArray = [
+  'Balloon',
+  'Composition',
+  'Post'
+]
 
 export type ExtraRatingTypesEnum = 'HeartRating' | 'StarRating'
-export const ExtraRatingTypesEnumArray = ['HeartRating', 'StarRating']
+export const ExtraRatingTypesEnumArray = [
+  'HeartRating',
+  'StarRating'
+]
 
-export type LocalesEnum =
-  | 'de-DE'
-  | 'en-AU'
-  | 'en-BZ'
-  | 'en-CA'
-  | 'en-cb'
-  | 'en-GB'
-  | 'en-IE'
-  | 'en-IN'
-  | 'en-JM'
-  | 'en-MT'
-  | 'en-MY'
-  | 'en-NZ'
-  | 'en-PH'
-  | 'en-SG'
-  | 'en-TT'
-  | 'en-US'
-  | 'en-ZA'
-  | 'en-ZW'
-  | 'es-ES'
-  | 'fr-FR'
-  | 'it-IT'
-  | 'ja-JP'
-  | 'ko-KR'
-  | 'pt-BR'
-  | 'zh-CN'
-  | 'zh-TW'
+export type LocalesEnum = 'de-DE' | 'en-AU' | 'en-BZ' | 'en-CA' | 'en-cb' | 'en-GB' | 'en-IE' | 'en-IN' | 'en-JM' | 'en-MT' | 'en-MY' | 'en-NZ' | 'en-PH' | 'en-SG' | 'en-TT' | 'en-US' | 'en-ZA' | 'en-ZW' | 'es-ES' | 'fr-FR' | 'it-IT' | 'ja-JP' | 'ko-KR' | 'pt-BR' | 'zh-CN' | 'zh-TW'
 export const LocalesEnumArray = [
   'de-DE',
   'en-AU',
@@ -69,14 +68,21 @@ export const LocalesEnumArray = [
   'ko-KR',
   'pt-BR',
   'zh-CN',
-  'zh-TW',
+  'zh-TW'
 ]
 
 export type LocalizableTypesEnum = 'Composition' | 'CompositionAsset'
-export const LocalizableTypesEnumArray = ['Composition', 'CompositionAsset']
+export const LocalizableTypesEnumArray = [
+  'Composition',
+  'CompositionAsset'
+]
 
 export type Species = 'cat' | 'dog' | 'frog'
-export const SpeciesArray = ['cat', 'dog', 'frog']
+export const SpeciesArray = [
+  'cat',
+  'dog',
+  'frog'
+]
 
 export interface BalloonLine {
   balloon: any
@@ -122,8 +128,10 @@ export interface Pet {
   species: Species
 }
 
-export interface PetUnderstudyJoinModel {}
+export interface PetUnderstudyJoinModel {
+}
 
 export interface Sandbag {
   weight: any
 }
+
