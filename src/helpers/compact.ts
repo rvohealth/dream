12ed72406959132be37fc04e7464ee9643dc04ac
Daniel Nelson <844258+daniel-nelson@users.noreply.github.com>
2024-02-26 11:34:34 -0600

    Properly type compact

--- src/helpers/compact.ts
+++ src/helpers/compact.ts
@@ -1,9 +1,16 @@
-export function compact(obj: any[]): any[]
-export function compact(obj: { [key: string]: any }): { [key: string]: any }
-export default function compact(obj: any): any {
+export default function compact<
+  T extends any,
+  R extends T extends (infer Item)[]
+    ? Exclude<Item, null | undefined>[]
+    : T extends { [key: string]: any }
+      ? CompactedObject<T>
+      : never,
+>(obj: T): R {
   if (Array.isArray(obj)) {
-    return obj.filter(val => ![undefined, null].includes(val)) as any[]
+    return obj.filter(val => ![undefined, null].includes(val as any)) as R
   } else {
-    return Object.fromEntries(Object.entries(obj).filter(([, v]) => v != null))
+    return Object.fromEntries(Object.entries(obj as any).filter(([, v]) => v != null)) as R
   }
 }
+
+type CompactedObject<T> = { [K in keyof T as T[K] extends null | undefined ? never : K]: T[K] }
