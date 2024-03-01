7f0b3037ab14a5c96d87603a2ba26e390dbfacf8
Daniel Nelson <844258+daniel-nelson@users.noreply.github.com>
2024-03-01 12:08:44 -0600

    associationUpdateQuery isn't applicable to the joins + updateAll use case. Recommend where + nestedSelect instead
    Addresses https://rvohealth.atlassian.net/browse/PDTC-4598

--- src/exceptions/no-updateall-on-joins.ts
+++ src/exceptions/no-updateall-on-joins.ts
@@ -1,7 +1,10 @@
 export default class NoUpdateAllOnJoins extends Error {
   public get message() {
     return `
-udpateAll may not be called on joins. Use associationUpdateQuery instead
-    `
+udpateAll may not yet be called on joins. As a workaround,
+use where + nestedSelect instead, e.g.:
+
+  ModelA.where({ id: ModelB.nestedSelect('modelAId') })
+`
   }
 }
