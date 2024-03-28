4ce74990b04163fabffec18bb5218e63a876738f
avocadojesus <jgarbey@gmail.com>
2024-03-28 16:46:57 -0700

    remove feature_spec_path

--- shared/helpers/path/featureSpecsPath.ts
+++
@@ -1,7 +0,0 @@
-import distOrProjectRootPath from './distOrProjectRootPath'
-import loadDreamYamlFile from './loadDreamYamlFile'
-
-export default async function featureSpecsPath({ omitDirname }: { omitDirname?: boolean } = {}) {
-  const yamlConfig = await loadDreamYamlFile()
-  return distOrProjectRootPath({ filepath: yamlConfig.feature_spec_path, omitDirname })
-}
