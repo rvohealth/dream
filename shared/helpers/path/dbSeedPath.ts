3fb7a6a448ad13e165710c0291d9a07ebf3cc93e
avocadojesus <jgarbey@gmail.com>
2024-03-28 16:45:24 -0700

    remove db_seed_path

--- shared/helpers/path/dbSeedPath.ts
+++ shared/helpers/path/dbSeedPath.ts
@@ -1,8 +1,12 @@
+import path from 'path'
 import distOrProjectRootPath from './distOrProjectRootPath'
 import loadDreamYamlFile from './loadDreamYamlFile'
 import transformExtension from './transformExtension'
 
 export default async function dbSeedPath({ omitDirname }: { omitDirname?: boolean } = {}) {
   const yamlConfig = await loadDreamYamlFile()
-  return distOrProjectRootPath({ filepath: transformExtension(yamlConfig.db_seed_path), omitDirname })
+  return distOrProjectRootPath({
+    filepath: transformExtension(path.join(yamlConfig.db_path, 'seed.ts')),
+    omitDirname,
+  })
 }
