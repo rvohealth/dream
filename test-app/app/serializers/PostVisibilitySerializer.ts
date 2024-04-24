4810c949e3acdb66a3c6b33e8f41755f84c9f457
avocadojesus <jgarbey@gmail.com>
2024-04-24 15:23:01 -0700

    always use serializer if passed in serializerCB

--- test-app/app/serializers/PostVisibilitySerializer.ts
+++ test-app/app/serializers/PostVisibilitySerializer.ts
@@ -1,15 +1,12 @@
 import RendersOne from '../../../src/serializer/decorators/associations/renders-one'
 import DreamSerializer from '../../../src/serializer'
-import Pet from '../models/Pet'
 import PostVisibility from '../models/PostVisibility'
+import Post from '../models/Post'
 
 // NOTE: this serializer is intentionally exported
 // non-default to test that our client api
 // generator can support it
 export class PostVisibilitySerializer<DataType extends PostVisibility> extends DreamSerializer<DataType> {
   @RendersOne()
-  public pet: Pet
-
-  @RendersOne()
-  public understudy: Pet
+  public post: Post
 }
