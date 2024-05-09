38fd4210541cb0bf1eccd2e9bb74fb5e2e83e3c7
avocadojesus <jgarbey@gmail.com>
2024-05-09 14:33:36 -0700

    catch exceptions on serializers getter
    We wanted these errors to flow through originally, but we also
    considered it a valid pattern to have a serializers getter that throws
    an exception when accessed. The problem is, when syncing client api
    schema, we can put ourselves in a situation where we often have to call
    the serializers getter to determine what the corresponding serializer to
    a model is.

--- test-app/app/serializers/UserSerializer.ts
+++ test-app/app/serializers/UserSerializer.ts
@@ -1,8 +1,10 @@
 import { IdType } from '../../../src/dream/types'
 import DreamSerializer from '../../../src/serializer'
 import Attribute from '../../../src/serializer/decorators/attribute'
+import RendersOne from '../../../src/serializer/decorators/associations/renders-one'
 import User from '../models/User'
 import { CalendarDate } from '../../../src'
+import UserSettings from '../models/UserSettings'
 
 export default class UserSerializer<
   DataType extends User,
@@ -16,6 +18,9 @@ export default class UserSerializer<
 
   @Attribute('date')
   public birthdate: CalendarDate
+
+  @RendersOne({ optional: true })
+  public userSettings: UserSettings
 }
 
 export class UserSummarySerializer<DataType extends User, Passthrough extends object> extends DreamSerializer<
