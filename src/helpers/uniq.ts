72629d79961a6b77d4960f664f2bf25ade5530d9
Daniel Nelson <844258+daniel-nelson@users.noreply.github.com>
2024-05-30 16:40:09 -0500

    Properly type uniq

--- src/helpers/uniq.ts
+++ src/helpers/uniq.ts
@@ -1,10 +1,10 @@
-import uniqWith from 'lodash.uniqwith'
 import lodashUniq from 'lodash.uniq'
+import uniqWith from 'lodash.uniqwith'
 import Dream from '../dream'
 
 export default function uniq<T>(
   arr: T[],
-  comparator: ((a: any, b: any) => boolean) | undefined = undefined
+  comparator: ((a: T, b: T) => boolean) | undefined = undefined
 ): T[] {
   if (comparator) return uniqWith(arr, comparator)
   else if ((arr[0] as Dream)?.isDreamInstance) return uniqWith(arr as Dream[], dreamComparator) as T[]
