da9b1dd15ab3711b969f0bbe574b7288bb38f33e
avocadojesus <jgarbey@gmail.com>
2024-07-10 10:37:46 -0700

    add comments to some decorators

--- src/decorators/associations/has-many.ts
+++ src/decorators/associations/has-many.ts
@@ -12,6 +12,40 @@ import {
   validateHasStatementArgs,
 } from './shared'
 
+/**
+ * Establishes a "HasMany" association between the base dream
+ * and the child dream, where the child dream has a foreign key
+ * which points back to the base dream. This relationship should
+ * always have a corresponding `@BelongsTo` association on the
+ * child class.
+ *
+ * ```ts
+ * class User extends ApplicationModel {
+ *   @HasMany(() => Post)
+ *   public posts: Post[]
+ * }
+ *
+ * class Post extends ApplicationModel {
+ *   @BelongsTo(() => User)
+ *   public user: User
+ *   public userId: DreamColumn<Post, 'userId'>
+ * }
+ * ```
+ *
+ * @param opts.dependent - Can be either "destroy" or undefined. If "destroy", this record will be cascade deleted if the base model is destroyed.
+ * @param opts.distinct - Can be a column name, or else a boolean. If a column name, a distinct clause will be applied to the column. If true, a distinct clause will be applied to the primary key.
+ * @param opts.foreignKey - A custom column name to use for the foreign key.
+ * @param opts.order - A custom order statement to apply to this association.
+ * @param opts.polymorphic - If true, this association will be treated as a polymorphic association.
+ * @param opts.preloadThroughColumns - An array of columns to pluck off the through association attached to this association. Can only be set if `through` is also set.
+ * @param opts.primaryKeyOverride - A custom column name to use for the primary key.
+ * @param opts.selfWhere - Adds a where clause to an association between a column on the associated model and a column on this model.
+ * @param opts.selfWhereNot - Adds a where not clause to an association between a column on the associated model and a column on this model.
+ * @param opts.source - Used in conjunction with 'through' to specify the source association on a child model.
+ * @param opts.through - If passed, this association will travel through another association.
+ * @param opts.where - A where clause to be applied when this association is loaded
+ * @param opts.whereNot - A where not clause to be applied when this association is loaded
+ */
 export default function HasMany<
   BaseInstance extends Dream = Dream,
   AssociationDreamClass extends typeof Dream = typeof Dream,
--- src/decorators/hooks/after-create-commit.ts
+++ src/decorators/hooks/after-create-commit.ts
@@ -1,6 +1,23 @@
 import Dream from '../../dream'
 import { HookStatement, blankHooksFactory, AfterHookOpts } from './shared'
 
+/**
+ * Calls the decorated method whenever a dream has finished
+ * being created. If the save takes place within a transaction,
+ * this method will not be called until the transaction is
+ * committed. However, if the save is not taking place in
+ * a transaction, the method will be run after the save
+ * is complete.
+ *
+ * class User extends ApplicationModel {
+ *   @AfterCreateCommit()
+ *   public doSomething() {
+ *     ...
+ *   }
+ * }
+ *
+ * @param opts.ifChanged - Optional. A list of columns which should must change in order for this function to be called.
+ */
 export default function AfterCreateCommit<T extends Dream | null = null>(opts: AfterHookOpts<T> = {}): any {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   return function (target: any, key: string, _: any) {
