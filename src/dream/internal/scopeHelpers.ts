5e2c4b909469f2d8abcf798798d5f59f5efc9d39
Daniel Nelson <844258+daniel-nelson@users.noreply.github.com>
2024-07-16 11:49:29 -0500

    fix

--- src/dream/internal/scopeHelpers.ts
+++ src/dream/internal/scopeHelpers.ts
@@ -10,6 +10,6 @@ export const DEFAULT_SKIP_HOOKS = false
 
 export function addSoftDeleteScopeToUserScopes<DreamInstance extends Dream>(
   userScopes: AllDefaultScopeNames<DreamInstance['dreamconf']>[] = []
-): AllDefaultScopeNames<DreamInstance['dreamconf']> {
-  return uniq([...userScopes, SOFT_DELETE_SCOPE_NAME]) as AllDefaultScopeNames<DreamInstance['dreamconf']>
+): AllDefaultScopeNames<DreamInstance['dreamconf']>[] {
+  return uniq([...userScopes, SOFT_DELETE_SCOPE_NAME] as AllDefaultScopeNames<DreamInstance['dreamconf']>)
 }
