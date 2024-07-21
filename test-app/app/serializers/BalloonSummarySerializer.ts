c56d3d101c6da6dbbd1bd6ac4cc41906e0ccaa1c
avocadojesus <jgarbey@gmail.com>
2024-07-21 15:38:04 -0700

    remove dead enum

--- test-app/app/serializers/BalloonSummarySerializer.ts
+++ test-app/app/serializers/BalloonSummarySerializer.ts
@@ -2,7 +2,7 @@ import DreamSerializer from '../../../src/serializer'
 import Attribute from '../../../src/serializer/decorators/attribute'
 
 export default class BalloonSummarySerializer extends DreamSerializer {
-  @Attribute('type:BalloonTypesEnum')
+  @Attribute('string')
   public type: string
 
   @Attribute()
