f4ee674f36e95a65ec62388f818db130087f0587
avocadojesus <jgarbey@gmail.com>
2024-07-24 17:50:48 -0700

    add default uspec path

--- src/helpers/path/unitSpecsPath.ts
+++ src/helpers/path/unitSpecsPath.ts
@@ -1,7 +1,6 @@
 import distOrProjectRootPath from './distOrProjectRootPath'
-import loadDreamYamlFile from './loadDreamYamlFile'
+import relativeDreamPath from './relativeDreamPath'
 
 export default async function unitSpecsPath({ omitDirname }: { omitDirname?: boolean } = {}) {
-  const yamlConfig = await loadDreamYamlFile()
-  return distOrProjectRootPath({ filepath: yamlConfig.unit_spec_path, omitDirname })
+  return distOrProjectRootPath({ filepath: await relativeDreamPath('uspec'), omitDirname })
 }
