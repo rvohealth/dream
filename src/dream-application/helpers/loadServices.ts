237b3e5baee35eb93c41c1958c30ccdfce39883c
avocadojesus <jgarbey@gmail.com>
2024-08-13 11:53:25 -0700

    raise exception if global service name conflict

--- src/dream-application/helpers/loadServices.ts
+++ src/dream-application/helpers/loadServices.ts
@@ -1,4 +1,5 @@
 import getFiles from '../../helpers/getFiles'
+import globalNameIsAvailable from './globalNameIsAvailable'
 
 let _services: Record<string, any>
 
@@ -12,6 +13,22 @@ export default async function loadServices(servicesPath: string): Promise<Record
     const service = (await import(servicePath)).default
     const serviceName = service.globalName || service.name
     if (serviceName) {
+      if (!globalNameIsAvailable(serviceName)) {
+        throw new Error(
+          `
+Attempted to register ${serviceName}, but something else was detected with the same
+name. To fix this, make sure the class name you use for this service is unique to your system.
+
+For services, you can specify a different name to register on by adding a static "globalName" getter:
+
+class ${serviceName} {
+  public static globalName() {
+    return 'MyCustomGlobalName'
+  }
+}`
+        )
+      }
+
       _services[serviceName] = service
     }
   }
