dbeedff09a2e21a9c166d47cb9bbd9eb794e3eb8
avocadojesus <jgarbey@gmail.com>
2024-08-13 10:56:26 -0700

    fix build

--- boot/build-docs.ts
+++ boot/build-docs.ts
@@ -2,10 +2,8 @@ import './cli/helpers/loadAppEnvFromBoot'
 
 import pack from '../package.json'
 import sspawn from '../src/helpers/sspawn'
-import initializeDreamApplication from '../test-app/cli/helpers/initializeDreamApplication'
 
 export default async function buildDocs() {
-  await initializeDreamApplication()
   console.log('generating docs for dream version: ' + pack.version + '...')
   await sspawn(`yarn typedoc src/index.ts --tsconfig ./tsconfig.build.json --out docs/${pack.version}`)
   console.log('done!')
