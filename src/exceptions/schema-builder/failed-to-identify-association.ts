92164150c435f587e25ae41db5ff6d696266c078
avocadojesus <jgarbey@gmail.com>
2024-08-14 21:03:02 -0700

    better message

--- src/exceptions/schema-builder/failed-to-identify-association.ts
+++ src/exceptions/schema-builder/failed-to-identify-association.ts
@@ -5,31 +5,37 @@ import { getCachedDreamApplicationOrFail } from '../../dream-application/cache'
 export default class FailedToIdentifyAssociation extends Error {
   constructor(
     private modelClass: typeof Dream,
-    private associationMetaData: AssociationStatement,
+    private associationType: string,
     private associationName: string
   ) {
     super()
   }
 
   public get message() {
+    const possibleMatches = Object.keys(getCachedDreamApplicationOrFail().models).filter(globalName =>
+      new RegExp(this.associationName.slice(0, 3)).test(globalName)
+    )
+
     return `
 An unexpected error occurred while looking up an association that you have defined.
 
-While building the schema for your app, we attempted to follow the ${this.associationMetaData.type} association
-on ${this.modelClass.name}. Usually, this is because the global name for the model is not what you anticipated,
-which often happens when you are using models that are in nested directories, since their names can be counter-intuitive.
+While building the schema for your app, we failed to find a match for
+the ${this.associationType} association "${this.associationName}"
+on ${this.modelClass.name}. Usually, this is because the global name for
+the model is not what you anticipated, which often happens when you are
+using models that are in nested directories, since their names can be
+counter-intuitive.
 
-We recommend that you lean into the autocomplete when providing the first argument to an association, since
-it can help to catch these familiar gotchas.
+We recommend that you lean into the autocomplete when providing the
+first argument to an association, since it can help to catch these
+familiar gotchas.
 
 Did you by chance mean one of these associations?:
-    ${Object.keys(getCachedDreamApplicationOrFail().models)
-      .filter(globalName => new RegExp(`^${this.associationName.slice(0, 5)}`).test(globalName))
-      .join(',\n    ')}
+    ${possibleMatches.join('\n    ')}
 
 Details:
     dream: ${this.modelClass.name}
-    association type: ${this.associationMetaData.type}
+    association type: ${this.associationType}
     association name: ${this.associationName}
 `
   }
