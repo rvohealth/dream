c0eb8e3b70858563ff1584a1417c20ffbf3a5fa1
Daniel Nelson <844258+daniel-nelson@users.noreply.github.com>
2024-08-20 16:37:26 -0500

    psychic-support-helpers

--- psychic-support-helpers/index.ts
+++ psychic-support-helpers/index.ts
@@ -1,4 +1,5 @@
 export { default as globalClassNameFromFullyQualifiedModelName } from '../src/helpers/globalClassNameFromFullyQualifiedModelName'
 export { default as relativeDreamPath } from '../src/helpers/path/relativeDreamPath'
+export { default as sharedPathPrefix } from '../src/helpers/path/sharedPathPrefix'
 export { default as serializerNameFromFullyQualifiedModelName } from '../src/helpers/serializerNameFromFullyQualifiedModelName'
 export { default as standardizeFullyQualifiedModelName } from '../src/helpers/standardizeFullyQualifiedModelName'
