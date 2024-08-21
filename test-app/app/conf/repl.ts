e645e3140f24ce852558c416fdfc80ecc89f9178
avocadojesus <jgarbey@gmail.com>
2024-08-21 03:13:24 -0700

    prune dead files, remove references to TS_SAFE

--- test-app/app/conf/repl.ts
+++ test-app/app/conf/repl.ts
@@ -1,8 +1,11 @@
-import { loadRepl } from '../../../src'
 import './loadEnv'
+
 import * as repl from 'node:repl'
+import { loadRepl } from '../../../src'
+import initializeDreamApplication from '../../cli/helpers/initializeDreamApplication'
 
 const replServer = repl.start('> ')
 export default (async function () {
+  await initializeDreamApplication()
   await loadRepl(replServer.context)
 })()
