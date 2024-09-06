982536874f2a13d07c766a03aef0fbfdefe2327b
avocadojesus <jgarbey@gmail.com>
2024-09-06 11:13:44 -0700

    fix global name loading in repl
    close https://rvohealth.atlassian.net/browse/PDTC-5994

--- src/helpers/loadRepl.ts
+++ src/helpers/loadRepl.ts
@@ -2,20 +2,24 @@ import './loadEnv'
 
 import { DateTime } from 'luxon'
 import DreamApplication from '../dream-application'
+import pascalizePath from './pascalizePath'
+import CalendarDate from './CalendarDate'
+import ops from '../ops'
 
-export default async function loadRepl(context: Record<string, unknown>) {
+export default function loadRepl(context: Record<string, unknown>) {
   const dreamApp = DreamApplication.getOrFail()
 
-  await dreamApp.inflections?.()
-
   context.DateTime = DateTime
+  context.CalendarDate = CalendarDate
+  context.ops = ops
+
   for (const globalName of Object.keys(dreamApp.models)) {
-    context[globalName] = dreamApp.models[globalName]
+    context[pascalizePath(globalName)] = dreamApp.models[globalName]
   }
 
   for (const globalName of Object.keys(dreamApp.services)) {
     if (!context[globalName]) {
-      context[globalName] = dreamApp.services[globalName]
+      context[pascalizePath(globalName)] = dreamApp.services[globalName]
     }
   }
 }
