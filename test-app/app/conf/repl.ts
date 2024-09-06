278edd3c345acc49d49dd282630307e83b7a706e
avocadojesus <jgarbey@gmail.com>
2024-09-06 11:13:44 -0700

    fix global name loading in repl
    close https://rvohealth.atlassian.net/browse/PDTC-5994

--- test-app/app/conf/repl.ts
+++ test-app/app/conf/repl.ts
@@ -7,5 +7,5 @@ import initializeDreamApplication from '../../cli/helpers/initializeDreamApplica
 const replServer = repl.start('> ')
 export default (async function () {
   await initializeDreamApplication()
-  await loadRepl(replServer.context)
+  loadRepl(replServer.context)
 })()
