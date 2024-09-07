1df71fd07e9f562c7c9d112b8c773e7bca5f9b05
Daniel Nelson <844258+daniel-nelson@users.noreply.github.com>
2024-09-07 17:37:17 -0500

    fix

--- test-app/cli/index.ts
+++ test-app/cli/index.ts
@@ -5,7 +5,7 @@
 // commanderjs docs:
 // https://github.com/tj/commander.js#quick-start
 
-import '../conf/app/loadEnv'
+import '../app/conf/loadEnv'
 
 import { Command } from 'commander'
 import { DreamCLI } from '../../src'
