abf0d8f92850892c0e0884e00e896ab641f89c54
avocadojesus <jgarbey@gmail.com>
2024-10-25 21:27:35 -0700

    add hashing support to Encrypt class

---
+++ spec/unit/encrypt/hash.spec.ts
@@ -0,0 +1,11 @@
+import { Encrypt } from '../../../src'
+
+describe('Encrypt.hash, Encrypt.veryify', () => {
+  it('can verify a string', async () => {
+    const hash = await Encrypt.hash('helloworld')
+    expect(hash).not.toEqual('helloworld')
+
+    expect(await Encrypt.verifyHash('helloworld', hash)).toBe(true)
+    expect(await Encrypt.verifyHash('invalid', hash)).toBe(false)
+  })
+})
