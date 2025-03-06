3c1f8a814f2e3394e6f8f86c5c5119f70da24f57
avocadojesus <jgarbey@gmail.com>
2025-03-06 15:49:08 -0800

    fixup

--- src/helpers/loadEnv.ts
+++ src/helpers/loadEnv.ts
@@ -4,16 +4,26 @@ import EnvInternal from './EnvInternal'
 import { fileURLToPath } from 'node:url'
 import { dirname } from 'node:path'
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
+declare const importMeta: unique symbol
+let finalDirname: string
+
+if (typeof importMeta !== 'undefined') {
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  const __filename = fileURLToPath(import.meta.url)
+  const __dirname = dirname(__filename)
+  finalDirname = __dirname
+} else {
+  finalDirname = __dirname
+}
 
 const fileName = `.env${EnvInternal.isTest ? '.test' : ''}`
 let dotenvpath: string = ''
 
 if (EnvInternal.boolean('DREAM_CORE_DEVELOPMENT')) {
   dotenvpath = EnvInternal.boolean('DREAM_CORE_SPEC_RUN')
-    ? __dirname + `/../../${fileName}`
-    : __dirname + `/../../../${fileName}`
+    ? finalDirname + `/../../${fileName}`
+    : finalDirname + `/../../../${fileName}`
 } else {
   dotenvpath = `../../${fileName}`
 }
