192f75951b60023d05bfdf42824fbeb430b806ad
Daniel Nelson <844258+daniel-nelson@users.noreply.github.com>
2025-03-15 10:26:25 -0500

    spec/unit/helpers/path/relativeDreamPath.spec.ts

--- spec/unit/helpers/path/relativeDreamPath.spec.ts
+++ spec/unit/helpers/path/relativeDreamPath.spec.ts
@@ -5,83 +5,83 @@ import relativeDreamPath, {
 describe('relativeDreamPath', () => {
   context('to models', () => {
     context('from serializers with a model name', () => {
-      it('returns ../models/<ModelName>', () => {
-        expect(relativeDreamPath('serializers', 'models', 'User')).toEqual('../models/User')
+      it('returns ../models/<ModelName>.js', () => {
+        expect(relativeDreamPath('serializers', 'models', 'User')).toEqual('../models/User.js')
       })
     })
 
     context('from factories with a model name', () => {
-      it('returns ../../app/models/<ModelName>', () => {
-        expect(relativeDreamPath('factories', 'models', 'User')).toEqual('../../app/models/User')
+      it('returns ../../app/models/<ModelName>.js', () => {
+        expect(relativeDreamPath('factories', 'models', 'User')).toEqual('../../app/models/User.js')
       })
     })
 
     context('from serializers with a nested model name', () => {
-      it('returns ../models/<NestedName>/<ModelName>', () => {
-        expect(relativeDreamPath('serializers', 'models', 'Graph/Edge')).toEqual('../../models/Graph/Edge')
+      it('returns ../models/<NestedName>/<ModelName>.js', () => {
+        expect(relativeDreamPath('serializers', 'models', 'Graph/Edge')).toEqual('../../models/Graph/Edge.js')
       })
     })
 
     context('from factories with a nested model name', () => {
-      it('returns ../../app/models/<NestedName>/<ModelName>', () => {
+      it('returns ../../app/models/<NestedName>/<ModelName>.js', () => {
         expect(relativeDreamPath('factories', 'models', 'Graph/Edge')).toEqual(
-          '../../../app/models/Graph/Edge'
+          '../../../app/models/Graph/Edge.js'
         )
       })
     })
 
     context('to models with a different origin and destination model', () => {
       context('from serializers with a model name', () => {
-        it('returns ../models/<DestinationModelName>', () => {
+        it('returns ../models/<DestinationModelName>.js', () => {
           expect(relativeDreamPath('serializers', 'models', 'User', 'Graph/Edge')).toEqual(
-            '../models/Graph/Edge'
+            '../models/Graph/Edge.js'
           )
         })
       })
 
       context('from factories with a model name', () => {
-        it('returns ../../app/models/<ModelName>', () => {
+        it('returns ../../app/models/<ModelName>.js', () => {
           expect(relativeDreamPath('factories', 'models', 'User', 'Graph/Edge')).toEqual(
-            '../../app/models/Graph/Edge'
+            '../../app/models/Graph/Edge.js'
           )
         })
       })
 
       context('from serializers with a nested model name', () => {
-        it('returns ../models/<NestedName>/<ModelName>', () => {
+        it('returns ../models/<NestedName>/<ModelName>.js', () => {
           expect(relativeDreamPath('serializers', 'models', 'Graph/Edge', 'User')).toEqual(
-            '../../models/User'
+            '../../models/User.js'
           )
         })
       })
 
       context('from factories with a nested model name', () => {
-        it('returns ../../app/models/<NestedName>/<ModelName>', () => {
+        it('returns ../../app/models/<NestedName>/<ModelName>.js', () => {
           expect(relativeDreamPath('factories', 'models', 'Graph/Edge', 'User')).toEqual(
-            '../../../app/models/User'
+            '../../../app/models/User.js'
           )
         })
       })
 
       context('from models with a model name', () => {
-        it('returns ./<NestedName>/<ModelName>', () => {
-          expect(relativeDreamPath('models', 'models', 'User', 'Graph/Edge')).toEqual('./Graph/Edge')
+        it('returns ./<NestedName>/<ModelName>.js', () => {
+          expect(relativeDreamPath('models', 'models', 'User', 'Graph/Edge')).toEqual('./Graph/Edge.js')
         })
       })
 
       context('from models with a nested model name', () => {
-        it('returns ../<ModelName>', () => {
-          expect(relativeDreamPath('models', 'models', 'Graph/Edge', 'User')).toEqual('../User')
+        it('returns ../<ModelName>.js', () => {
+          expect(relativeDreamPath('models', 'models', 'Graph/Edge', 'User')).toEqual('../User.js')
         })
       })
     })
 
     context('from model with a nested model name to a model in the same directory', () => {
-      it('returns ./<ModelName>', () => {
-        expect(relativeDreamPath('models', 'models', 'Graph/Edge', 'Graph/Base')).toEqual('./Base')
-        expect(relativeDreamPath('models', 'models', 'Graph/Edge/Hello', 'Graph/Base')).toEqual('../Base')
+      it('returns ./<ModelName>.js', () => {
+        expect(relativeDreamPath('models', 'models', 'Graph/Edge', 'Graph/Base')).toEqual('./Base.js')
+        expect(relativeDreamPath('models', 'models', 'Graph/Edge/Hello', 'Graph/Base')).toEqual('../Base.js')
         expect(relativeDreamPath('models', 'models', 'Graph/Edge', 'Graph/Hello/World')).toEqual(
-          './Hello/World'
+          './Hello/World.js'
         )
       })
     })
@@ -89,29 +89,29 @@ describe('relativeDreamPath', () => {
 
   context('to serializers', () => {
     context('from models with a model name', () => {
-      it('returns ../serializers/<ModelName>Serializer', () => {
-        expect(relativeDreamPath('models', 'serializers', 'User')).toEqual('../serializers/UserSerializer')
+      it('returns ../serializers/<ModelName>Serializer.js', () => {
+        expect(relativeDreamPath('models', 'serializers', 'User')).toEqual('../serializers/UserSerializer.js')
       })
     })
 
     context('from models with a nested model name', () => {
-      it('returns ../serializers/<NestedName>/<ModelName>Serializer', () => {
+      it('returns ../serializers/<NestedName>/<ModelName>Serializer.js', () => {
         expect(relativeDreamPath('models', 'serializers', 'Graph/Edge')).toEqual(
-          '../../serializers/Graph/EdgeSerializer'
+          '../../serializers/Graph/EdgeSerializer.js'
         )
       })
     })
 
     context('from serializer with a nested model name to a serializer in the same directory', () => {
-      it('returns ./<ModelName>Serializer', () => {
+      it('returns ./<ModelName>Serializer.js', () => {
         expect(relativeDreamPath('serializers', 'serializers', 'Graph/Edge', 'Graph/Base')).toEqual(
-          './BaseSerializer'
+          './BaseSerializer.js'
         )
         expect(relativeDreamPath('serializers', 'serializers', 'Graph/Edge/Hello', 'Graph/Base')).toEqual(
-          '../BaseSerializer'
+          '../BaseSerializer.js'
         )
         expect(relativeDreamPath('serializers', 'serializers', 'Graph/Edge', 'Graph/Hello/World')).toEqual(
-          './Hello/WorldSerializer'
+          './Hello/WorldSerializer.js'
         )
       })
     })
@@ -119,13 +119,13 @@ describe('relativeDreamPath', () => {
 
   context('to db', () => {
     context('from models with a model name', () => {
-      it('returns ../serializers/<ModelName>Serializer', () => {
+      it('returns ../serializers/<ModelName>Serializer.js', () => {
         expect(relativeDreamPath('models', 'db', 'User')).toEqual('../../db/')
       })
     })
 
     context('from models with a nested model name', () => {
-      it('returns ../serializers/<NestedName>/<ModelName>Serializer', () => {
+      it('returns ../serializers/<NestedName>/<ModelName>Serializer.js', () => {
         expect(relativeDreamPath('models', 'db', 'Graph/Edge')).toEqual('../../../db/')
       })
     })
@@ -133,15 +133,17 @@ describe('relativeDreamPath', () => {
 
   context('to factories', () => {
     context('from models with a model name', () => {
-      it('returns ../../spec/factories/<ModelName>Factory', () => {
-        expect(relativeDreamPath('models', 'factories', 'User')).toEqual('../../spec/factories/UserFactory')
+      it('returns ../../spec/factories/<ModelName>Factory.js', () => {
+        expect(relativeDreamPath('models', 'factories', 'User')).toEqual(
+          '../../spec/factories/UserFactory.js'
+        )
       })
     })
 
     context('from models with a nested model name', () => {
-      it('returns ../factories/<NestedName>/<ModelName>Factory', () => {
+      it('returns ../factories/<NestedName>/<ModelName>Factory.js', () => {
         expect(relativeDreamPath('models', 'factories', 'Graph/Edge')).toEqual(
-          '../../../spec/factories/Graph/EdgeFactory'
+          '../../../spec/factories/Graph/EdgeFactory.js'
         )
       })
     })
