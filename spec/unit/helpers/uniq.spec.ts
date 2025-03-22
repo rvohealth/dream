7fc1e03bf09592f3d6b68b00667d9153be165261
Daniel Nelson <844258+daniel-nelson@users.noreply.github.com>
2025-03-22 14:12:16 -0500

    Make uniq more robust

--- spec/unit/helpers/uniq.spec.ts
+++ spec/unit/helpers/uniq.spec.ts
@@ -1,72 +1,71 @@
 import uniq from '../../../src/helpers/uniq.js'
+import Mylar from '../../../test-app/app/models/Balloon/Mylar.js'
 import GraphNode from '../../../test-app/app/models/Graph/Node.js'
 
 describe('uniq', () => {
-  const subject = () => uniq(array(), toKey)
-  let toKey: ((a: any) => string) | undefined
-
-  let item1: any
-  let item2: any
-  let item1Reloaded: any
-  const array = () => [item1, item2, item1Reloaded]
-
-  beforeEach(() => {
-    toKey = undefined
-  })
-
-  /**
-   *
-   *
-   *
-   * What about simple objects? Simple objects always compare as not equal
-   * This also needs to test Range, ops, DateTime, CalendarDate
-   * First test that objects are the same constructor type (instanceof, if possible)
-   * if not, then not the same
-   * if have .equals, then call that (dream, DateTime, CalendarDate, Range, ops)
-   * otherwise, convert to JSON and compare strings? or recursively check everything in the object?
-   * or do we just want objects to be compared based on whether they are the actual same object?
-   * Maybe that
-   *
-   *
-   *
-   *
-   */
-
   context('when the first element is a Dream', () => {
-    beforeEach(async () => {
-      item1 = await GraphNode.create({ name: 'Hello' })
-      item2 = await GraphNode.create({ name: 'Hello' })
-      item1Reloaded = await GraphNode.find(item1.id)
-    })
+    beforeEach(async () => {})
 
-    it('uses the custom Dream comparator', () => {
-      expect(subject()).toMatchObject([item1, item2])
+    it('uses the custom Dream comparator', async () => {
+      const graphNode1 = await GraphNode.create({ name: 'Hello' })
+      const graphNode2 = await GraphNode.create({ name: 'Hello' })
+      const graphNode3 = await GraphNode.find(graphNode1.id)
+
+      expect(uniq([graphNode1, graphNode2, graphNode3])).toMatchObject([graphNode1, graphNode2])
     })
 
     context('when a custom comparator is passed', () => {
-      it('uses the custom comparator', () => {
-        toKey = a => a.name
-        expect(subject()).toMatchObject([item1])
+      it('still uses the Dream comparator', async () => {
+        const graphNode1 = await GraphNode.create({ name: 'Hello' })
+        const graphNode2 = await GraphNode.create({ name: 'Hello' })
+        const graphNode3 = await GraphNode.findOrFail(graphNode1.id)
+
+        expect(uniq([graphNode1, graphNode2, graphNode3], (a: GraphNode) => a.name!)).toMatchObject([
+          graphNode1,
+          graphNode2,
+        ])
+      })
+    })
+
+    context('different types of Dreams', () => {
+      it('uses the custom comparator', async () => {
+        const graphNode1 = await GraphNode.create({ name: 'Hello' })
+        const balloon = await Mylar.create()
+        const graphNode2 = await GraphNode.findOrFail(graphNode1.id)
+
+        expect(uniq([graphNode1, balloon, graphNode2], (a: GraphNode) => a.name!)).toMatchObject([
+          graphNode1,
+          balloon,
+        ])
       })
     })
   })
 
   context('when the first element is not a Dream', () => {
-    beforeEach(() => {
-      item1 = 'world'
-      item2 = 'hello'
-      item1Reloaded = 'hello'
-    })
-
-    it('uses the standard lodash comparator', () => {
-      expect(subject()).toMatchObject([item1, item2])
+    it('it compares the items directly', () => {
+      expect(uniq(['world', 'hello', 'hello'])).toMatchObject(['world', 'hello'])
     })
 
     context('when a custom comparator is passed', () => {
       it('uses the custom comparator', () => {
-        toKey = a => a.constructor.name
-        expect(subject()).toMatchObject([item1])
+        expect(uniq(['world', 'hello', 'hello', ''], (str: string) => str.length)).toMatchObject([
+          'world',
+          '',
+        ])
       })
     })
+  })
+
+  it('supports mixed types', async () => {
+    const graphNode1 = await GraphNode.create({ name: 'Hello' })
+    const graphNode2 = await GraphNode.findOrFail(graphNode1.id)
+
+    expect(uniq([graphNode1, 'world', 3, 7, graphNode2, 'hello', 7, 'hello'])).toMatchObject([
+      graphNode1,
+      'world',
+      3,
+      7,
+      'hello',
+    ])
   })
 })
--- src/helpers/uniq.ts
+++ src/helpers/uniq.ts
@@ -1,23 +1,32 @@
 import Dream from '../Dream.js'
 
-export default function uniq<T>(arr: T[], toKey: ((a: T) => string) | undefined = undefined): T[] {
+export default function uniq<
+  ArrayType extends any[],
+  ElementType extends ArrayType extends (infer U)[] ? U : never,
+>(arr: ArrayType, toKey: ((a: ElementType) => string | number) | undefined = undefined): ArrayType {
   if (toKey) return uniqWith(arr, toKey)
-  else if ((arr[0] as Dream)?.isDreamInstance) return uniqWith(arr as Dream[], dreamKey) as T[]
-  else return Array.from(new Set(arr))
+  return uniqWith(arr, a => String(a))
 }
 
 function dreamKey(dream: Dream): string {
   return `${(dream.constructor as typeof Dream).globalName}:${dream.primaryKeyValue}`
 }
 
-function uniqWith<T>(arr: T[], toKey: (a: T) => string): T[] {
-  const map: Record<string, T> = arr.reduce(
+function uniqWith<ArrayType extends any[], ElementType extends ArrayType extends (infer U)[] ? U : never>(
+  arr: ArrayType,
+  toKey: (a: ElementType) => string | number
+): ArrayType {
+  const map: Record<string, ElementType> = arr.reduce(
     (acc, val) => {
-      acc[toKey(val)] ||= val
+      // Prefix with underscore to ensure that the values cannot be interpreted as integers.
+      // If they can be interpreted as integers, then the keys are ordered not by the
+      // order in which they were added, but in ascending numerical order.
+      const key = (val as unknown as Dream)?.isDreamInstance ? dreamKey(val) : `_${toKey(val)}`
+      acc[key] ||= val
       return acc
     },
-    {} as Record<string, T>
+    {} as Record<string, ElementType>
   )
 
-  return Object.values(map)
+  return Object.values(map) as ArrayType
 }
