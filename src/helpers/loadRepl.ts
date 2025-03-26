e2a183b1cf7ec41f44f34a955c6330f6babf36db
avocadojesus <jgarbey@gmail.com>
2025-03-26 16:25:45 -0700

    add missing helpers to repl

--- src/helpers/loadRepl.ts
+++ src/helpers/loadRepl.ts
@@ -4,16 +4,41 @@ import DreamApplication from '../dream-application/index.js'
 import Encrypt from '../encrypt/index.js'
 import ops from '../ops/index.js'
 import CalendarDate from './CalendarDate.js'
+import camelize from './camelize.js'
+import capitalize from './capitalize.js'
+import compact from './compact.js'
 import { DateTime } from './DateTime.js'
+import hyphenize from './hyphenize.js'
+import pascalize from './pascalize.js'
 import pascalizePath from './pascalizePath.js'
+import range from './range.js'
+import round from './round.js'
+import snakeify from './snakeify.js'
+import sort from './sort.js'
+import sortBy from './sortBy.js'
+import uncapitalize from './uncapitalize.js'
+import uniq from './uniq.js'
 
 export default function loadRepl(context: Record<string, unknown>) {
   const dreamApp = DreamApplication.getOrFail()
 
-  context.DateTime = DateTime
   context.CalendarDate = CalendarDate
+  context.DateTime = DateTime
   context.Encrypt = Encrypt
+  context.camelize = camelize
+  context.capitalize = capitalize
+  context.compact = compact
+  context.hyphenize = hyphenize
   context.ops = ops
+  context.pascalize = pascalize
+  context.pascalizePath = pascalizePath
+  context.range = range
+  context.round = round
+  context.snakeify = snakeify
+  context.sort = sort
+  context.sortBy = sortBy
+  context.uncapitalize = uncapitalize
+  context.uniq = uniq
 
   for (const globalName of Object.keys(dreamApp.models)) {
     context[pascalizePath(globalName)] = dreamApp.models[globalName]
