7b2c1e9f30e9d696c40b5bc76c5604aff52363ca
avocadojesus <jgarbey@gmail.com>
2025-03-28 12:41:05 -0700

    remove unused helper

--- src/helpers/dreamOrPsychicCoreDevelopment.ts
+++
@@ -1,9 +0,0 @@
-// normally we wouldn't consider psychic core development,
-// but given the awkward nature of the test-app folder,
-
-import EnvInternal from './EnvInternal.js'
-
-// it is sometimes necessary to consider it
-export default function dreamOrPsychicCoreDevelopment() {
-  return EnvInternal.boolean('DREAM_CORE_DEVELOPMENT') || EnvInternal.boolean('PSYCHIC_CORE_DEVELOPMENT')
-}
