a25587dc53f9fcc89406b0a5dd4ace44b570821a
avocadojesus <jgarbey@gmail.com>
2025-04-02 01:14:32 -0700

    remove boilerplate folder

--- boilerplate/cli/helpers/initializeDreamApplication.ts
+++
@@ -1,6 +0,0 @@
-import { DreamApplication } from '@rvoh/dream'
-import dreamConfCb from '../../app/conf/dream.js'
-
-export default async function initializeDreamApplication() {
-  return await DreamApplication.init(dreamConfCb)
-}
--- boilerplate/conf/loadEnv.ts
+++
@@ -1,4 +0,0 @@
-import dotenv from 'dotenv'
-
-const filePath = `./.env${process.env.NODE_ENV === 'test' ? '.test' : ''}`
-dotenv.config({ path: filePath })
