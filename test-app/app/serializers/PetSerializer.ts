dcbbe521382fee49aa3df71cb88fa82975a973c3
Daniel Nelson <844258+daniel-nelson@users.noreply.github.com>
2025-05-11 17:05:53 -0500

    wip

--- test-app/app/serializers/PetSerializer.ts
+++ test-app/app/serializers/PetSerializer.ts
@@ -1,8 +1,8 @@
-import DreamSerializer, { DreamSerializerBuilder } from '../../../src/serializer/index.js'
+import DreamSerializer from '../../../src/serializer/index.js'
 import Pet from '../models/Pet.js'
 
-// @DreamSerializer() // necessary to inject the name "PetSerializer" into the serializer for OpenAPI
-const PetSerializer = ($data: Pet, $passthroughData: object): DreamSerializerBuilder<Pet, object> =>
+// @Serializer() // necessary to inject the name "PetSerializer" into the serializer for OpenAPI
+const PetSerializer = ($data: Pet, $passthroughData: object) =>
   DreamSerializer($data, $passthroughData)
     .attribute('id')
     .attribute('name')
@@ -13,6 +13,6 @@ const PetSerializer = ($data: Pet, $passthroughData: object): DreamSerializerBui
 
 export default PetSerializer
 
-// @DreamSerializer() // necessary to inject the name "PetSummarySerializer" into the serializer for OpenAPI
-export const PetSummarySerializer = (DataType: Pet, Passthrough: object): DreamSerializerBuilder<Pet, object> =>
+// @Serializer() // necessary to inject the name "PetSummarySerializer" into the serializer for OpenAPI
+export const PetSummarySerializer = (DataType: Pet, Passthrough: object) =>
   DreamSerializer(DataType, Passthrough).attribute('id').attribute('favoriteTreats')
