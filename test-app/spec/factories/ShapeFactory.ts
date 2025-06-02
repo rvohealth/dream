30d9bace612c8410f0b95ebac3990592a91333b8
Trace Helms <tracehelms@gmail.com>
2025-06-02 16:42:13 -0700

    fix shape factory

--- test-app/spec/factories/ShapeFactory.ts
+++ test-app/spec/factories/ShapeFactory.ts
@@ -6,7 +6,7 @@ let counter = 0
 export default async function createShape(attrs: UpdateableProperties<Shape> = {}) {
   return await Shape.create({
     name: `Shape name ${++counter}`,
-    type: 'regular',
+    type: 'RegularShape',
     ...attrs,
   })
 }
