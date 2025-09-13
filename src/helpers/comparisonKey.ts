a2bdabdd8b196869adcca848530f3d167d063bfb
Daniel Nelson <844258+daniel-nelson@users.noreply.github.com>
2025-09-13 13:53:18 -0500

    Leverage Map instead of an object so don't need to prefix numeric keys with underscore to prevent them from being sorted by numeric value rather than by the order in which they were added to the map

--- src/helpers/comparisonKey.ts
+++ src/helpers/comparisonKey.ts
@@ -3,6 +3,19 @@ import Dream from '../Dream.js'
 export default function comparisonKey<ElementType>(
   val: ElementType,
   toKey: ((a: ElementType) => string | number | bigint) | undefined = undefined
-) {
-  return val instanceof Dream ? val.comparisonKey : toKey ? toKey(val) : val
+): number | string | bigint | null | undefined {
+  if (val instanceof Dream) return val.comparisonKey
+  if (toKey) return toKey(val)
+
+  if (val === null) return null
+  if (val === undefined) return undefined
+
+  switch (typeof val) {
+    case 'number':
+    case 'string':
+    case 'bigint':
+      return val
+    default:
+      return val.toString()
+  }
 }
--- src/helpers/uniq.ts
+++ src/helpers/uniq.ts
@@ -4,18 +4,11 @@ export default function uniq<
   ArrayType extends any[],
   ElementType extends ArrayType extends (infer U)[] ? U : never,
 >(arr: ArrayType, toKey: ((a: ElementType) => string | number | bigint) | undefined = undefined): ArrayType {
-  const map: Record<string, ElementType> = arr.reduce(
-    (acc, val) => {
-      const baseKey = comparisonKey(val, toKey)
-      // Prefix with underscore to ensure that the values cannot be _interpreted_ as integers.
-      // If they can be interpreted as integers, then the keys are ordered not by the
-      // order in which they were added, but in ascending numerical order.
-      const key = typeof baseKey === 'string' ? baseKey : `_${baseKey}`
-      acc[key] ||= val
-      return acc
-    },
-    {} as Record<string, ElementType>
-  )
+  const map = arr.reduce((acc: Map<any, ElementType>, val) => {
+    const key = comparisonKey(val, toKey)
+    if (!acc.has(key)) acc.set(key, val)
+    return acc
+  }, new Map())
 
-  return Object.values(map) as ArrayType
+  return [...map.values()] as ArrayType
 }
