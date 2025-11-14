e94fba74728d55da14b5ba2b5e34ffae4152994a
Daniel Nelson <844258+daniel-nelson@users.noreply.github.com>
2025-11-14 14:07:29 -0600

    Remove test-app/spec/factories

--- test-app/spec/factories/Sti/BFactory.ts
+++
@@ -1,8 +0,0 @@
-import { UpdateableProperties } from '@rvoh/dream/types'
-import StiB from '@models/Sti/B.js'
-
-export default async function createStiB(attrs: UpdateableProperties<StiB> = {}) {
-  return await StiB.create({
-    ...attrs,
-  })
-}
