670b1bf2d67632bce36e181895681b6e24f0e9b1
avocadojesus <fgarbutt@rvohealth.com>
2026-03-06 16:40:01 -0800

    fix relative path imports
    when nextjs houses a dream app, it is possible for the dream app to mis-interpret the relative paths provided and miscalculate the global name

--- src/dream-app/helpers/globalModelKeyFromPath.ts
+++ src/dream-app/helpers/globalModelKeyFromPath.ts
@@ -1,8 +1,9 @@
+import * as path from 'node:path'
 import normalizeFilePath from './normalizeFilePath.js'
 
 export default function (filepath: string, dirPath: string) {
-  const normalizedDirPath = normalizeFilePath(dirPath)
-  const normalizedFilepath = normalizeFilePath(filepath)
+  const normalizedDirPath = normalizeFilePath(path.resolve(dirPath))
+  const normalizedFilepath = normalizeFilePath(path.resolve(filepath))
   const prefixPath = normalizedDirPath
   return normalizedFilepath
     .replace(prefixPath, '')
